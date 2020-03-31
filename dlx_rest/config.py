bf3c303cae9ce0caefe86580cdbfd1472529acb5
Aaron Helton <mariusagricola@gmail.com>
2020-03-31 15:49:25 -0400

    ditching cognito

--- dlx_rest/config.py
+++ dlx_rest/config.py
@@ -8,25 +8,17 @@ class Config(object):
     TESTING = False
     
     if 'DLX_REST_TESTING' in os.environ:
-        client = boto3.client('ssm')
+        client = None
         connect_string = 'mongomock://localhost'
         TESTING = True
-        cognito_pool_id = client.get_parameter(Name='dev-dlx-cognito-pool-id')['Parameter']['Value']
-        cognito_client_id = client.get_parameter(Name='dev-dlx-cognito-client-id')['Parameter']['Value']
-        cognito_signin_url = 'https://dev-dlx-rest.auth.us-east-1.amazoncognito.com'
     elif 'DLX_REST_DEV' in os.environ:
         client = boto3.client('ssm')
         connect_string = client.get_parameter(Name='dlx-dev-connect-string')['Parameter']['Value']
-        cognito_pool_id = client.get_parameter(Name='dev-dlx-cognito-pool-id')['Parameter']['Value']
-        cognito_client_id = client.get_parameter(Name='dev-dlx-cognito-client-id')['Parameter']['Value']
     elif 'DLX_REST_PRODUCTION' in os.environ:
         client = boto3.client('ssm')
         connect_string = client.get_parameter(Name='dlx-prod-connect-string')['Parameter']['Value']
-        cognito_pool_id = client.get_parameter(Name='dlx-cognito-pool-id')['Parameter']['Value']
-        cognito_client_id = client.get_parameter(Name='dlx-cognito-client-id')['Parameter']['Value']
     else:
         raise Exception('One of the environment variables "DLX_REST_TESTING", "DLX_REST_DEV", or "DLX_REST_PRODUCTION" must return a true value in order to initialize the runtime environment')
 
-    cognito_pool_url = 'https://cognito-idp.us-east-1.amazonaws.com'
     BIB_COLLECTION = 'bibs'
     AUTH_COLLECTION = 'auths'
--- dlx_rest/routes.py
+++ dlx_rest/routes.py
@@ -1,58 +1,14 @@
 from functools import wraps
-from flask import url_for, Flask, abort, g, jsonify, request
+from flask import url_for, Flask, abort, g, jsonify, request, redirect
 from urllib import request as web_request
 from jose import jwt
 from dlx_rest.app import app
+from dlx_rest.config import Config
 from dlx_rest.models import User
 
-# Utility Functions
-def authentication_required(view):
-    '''
-    Use this to wrap your authenticated functions.
-    '''
-    @wraps(view)
-    def wrap(*args, **kwargs):
-        token = request.headers.get('Authorization')
-        auth_error = (jsonify({'message': 'User not loggeed in. Please authenticate.'}))
-
-        if not token:
-            return auth_error
-        
-        user = get_identity(token)
-
-        if user:
-            request.user = user
-            return view(*args, **kwargs)
-        else:
-            return auth_error
-    return wrap
-
-def get_identity(jwt_token):
-    payload = decode_token(jwt_token)
-    user = User.objects(username=payload['cognito:username']).first()
-    if not user:
-        new_user = User(username=payload['cognito:username'])
-        new_user.save()
-        user = User.objects(username=payload['cognito:username']).first()
-    return user
-
-def decode_token(jwt_token):
-    # To do: set up Cognito pool and add it to config
-    jwks = web_request.urlopen(Config.cognito_pool_id + '/.well-known/jwks.json')
-    issuer = Config.cognito_pool_url + '/' + Config.cognito_pool_id
-    audience = Config.cognito_client_id
-
-    payload = jwt.decode(
-        jwt_token,
-        jwks.read(),
-        algorithms=['RS256'],
-        audience=audience,
-        issuer=issuer,
-        options={'verify_at_hash': False}
-    )
-
-    return payload
-
+# To do: 
+# Implement flask-login
+# Implement an authentication_required wrapper for flask-login
 
 # Main app routes
 @app.route('/')
@@ -67,7 +23,7 @@ def register():
 
 @app.route('/login')
 def login():
-    pass
+    return redirect(aws_auth.get_sign_in_url())
 
 @app.route('/logout')
 #@authentication_required
