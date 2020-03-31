8336b0fed1e6e021136cb400b7b4556a3db07844
Aaron Helton <mariusagricola@gmail.com>
2020-03-31 10:03:39 -0400

    build out test_app.py and finish preliminary auth

--- dlx_rest/routes.py
+++ dlx_rest/routes.py
@@ -30,27 +30,70 @@ def authentication_required(view):
 def get_identity(jwt_token):
     payload = decode_token(jwt_token)
     user = User.objects(username=payload['cognito:username']).first()
+    if not user:
+        new_user = User(username=payload['cognito:username'])
+        new_user.save()
+        user = User.objects(username=payload['cognito:username']).first()
+    return user
 
+def decode_token(jwt_token):
+    # To do: set up Cognito pool and add it to config
+    jwks = web_request.urlopen(Config.cognito_pool + '/.well-known/jwks.json')
+    issuer = Config.cognito_pool_url + '/' + Config.cognito_pool_id
+    audience = Config.client_id
+
+    payload = jwt.decode(
+        jwt_token,
+        jwks.read(),
+        algorithms=['RS256'],
+        audience=audience,
+        issuer=issuer,
+        options={'verify_at_hash': False}
+    )
+
+    return payload
 
 
 # Main app routes
 @app.route('/')
 def index():
-    return jsonify({"foo": "bar"})
+    pass
 
 
 # Users
 @app.route('/register')
 def register():
-    return jsonify({"foo": "bar"})
+    pass
 
 @app.route('/login')
 def login():
-    return jsonify({"foo": "bar"})
+    pass
 
 @app.route('/logout')
 @authentication_required
 def logout():
-    return jsonify({"foo": "bar"})
+    pass
 
+
+# Users Admin
+# Not sure if we should make any of this available to the API
+@app.route('/users')
+@authentication_required
+def list_users():
+    pass
+
+@app.route('/users/new', methods=['GET','POST'])
+@authentication_required
+def create_user():
+    pass
+
+@app.route('/users/<id>/edit', methods=['GET','POST'])
+@authentication_required
+def update_user(id):
+    pass
+
+@app.route('/users/<id>/delete', methods=['POST'])
+@authentication_required
+def delete_user(id):
+    pass
 
--- dlx_rest/tests/test_app.py
+++ dlx_rest/tests/test_app.py
@@ -10,10 +10,12 @@ def test_testing():
     assert Config.TESTING == True
     assert Config.connect_string == 'mongomock://localhost'
 
+# Index page
 def test_index(client):
     response = client.get(PRE)
     assert response.status_code == 200
 
+# User session management
 def test_register(client):
     response = client.get(PRE + '/register')
     assert response.status_code == 200
@@ -26,9 +28,32 @@ def test_logout(client):
     response = client.get(PRE + '/logout')
     assert response.status_code == 200
 
+# User administration
+def test_list_users(client):
+    response = client.get(PRE + '/users')
+    assert response.status_code == 200
+
+def test_create_user(client):
+    # To do: mock a new user, /users/1
+    response = client.get(PRE + '/users/new')
+    assert response.status_code == 200
+
+def test_update_user(client):
+    # To do: Users fixture, POST data
+    response = client.get(PRE + '/users/1/edit')
+    assert response.status_code == 200
+    response = client.post(PRE + '/users/1/edit')
+    assert response.status_code == 200
+
+def test_delete_user(client):
+    response = client.post(PRE + '/users/1/delete')
+    assert response.status_code == 200
+
+# Records
 def test_get_records_list(client):
     for col in ['bibs','auths']:
         response = client.get(PRE + '/records/{}'.format(col))
         assert response.status_code == 200
 
 #def test_get_record()
+
