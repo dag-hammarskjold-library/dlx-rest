6d16dd363fbc85ae6495b0e107fb8c84c0543d84
Aaron Helton <mariusagricola@gmail.com>
2020-04-28 09:52:17 -0400

    update prod connect details

--- dlx_rest/config.py
+++ dlx_rest/config.py
@@ -20,8 +20,9 @@ class Config(object):
     elif 'DLX_REST_PRODUCTION' in os.environ:
         client = boto3.client('ssm')
         secret_key = client.get_parameter(Name='metadata_cache_key')['Parameter']['Value']
-        connect_string = client.get_parameter(Name='dlx-prod-connect-string')['Parameter']['Value']
-        dbname = 'dlx'
+        #connect_string = client.get_parameter(Name='dlx-prod-connect-string')['Parameter']['Value']
+        connect_string = client.get_parameter(Name='connect-string')['Parameter']['Value']
+        dbname = 'undlFiles'
     else:
         raise Exception('One of the environment variables "DLX_REST_TESTING", "DLX_REST_DEV", or "DLX_REST_PRODUCTION" must return a true value in order to initialize the runtime environment')
 
