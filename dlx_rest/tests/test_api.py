ea03cb92ddaba887acb4265278d41a66c5607681
jbukhari <joe.bukhari@gmail.com>
2020-09-28 10:14:25 -0400

    enable post and put on records with data in mrk format

--- dlx_rest/tests/test_api.py
+++ dlx_rest/tests/test_api.py
@@ -12,6 +12,8 @@ if Config.connect_string != 'mongomock://localhost':
     raise Exception
 
 # tests
+
+#setup()
     
 def test_testing():
     assert Config.TESTING == True
@@ -21,61 +23,61 @@ def test_app(client):
     assert type(client).__name__ == 'FlaskClient'
 
 def test_collections_list(client):
-    data = json.loads(client.get(PRE+'/collections').data)
+    data = json.loads(client.get(f'{PRE}/collections').data)
     assert len(data['results']) == 2
-    assert PRE+'/bibs' in data['results']
-    assert PRE+'/auths' in data['results']
+    assert f'{PRE}/bibs' in data['results']
+    assert f'{PRE}/auths' in data['results']
     
-def test_records_list(client,records):
-    data = json.loads(client.get(PRE+'/bibs').data)
+def test_records_list(client, records):
+    data = json.loads(client.get(f'{PRE}/bibs').data)
     assert len(data['results']) == 50
     
-    data = json.loads(client.get(PRE+'/auths').data)
+    data = json.loads(client.get(f'{PRE}/auths').data)
     assert len(data['results']) == 50
     
-    data = json.loads(client.get(PRE+'/bibs?sort=date&direction=desc').data)
+    data = json.loads(client.get(f'{PRE}/bibs?sort=date&direction=desc').data)
     assert len(data['results']) == 50
-    assert data['results'][0] == PRE+'/bibs/50'
+    assert data['results'][0] == f'{PRE}/bibs/50'
     
-    data = json.loads(client.get(PRE+'/bibs?sort=date&direction=asc').data)
-    assert data['results'][0] == PRE+'/bibs/1'
+    data = json.loads(client.get(f'{PRE}/bibs?sort=date&direction=asc').data)
+    assert data['results'][0] == f'{PRE}/bibs/1'
     
-    response = client.get(PRE+'/bibs?format=xml')
+    response = client.get(f'{PRE}/bibs?format=xml')
     assert response.headers["Content-Type"] == 'text/xml; charset=utf-8'
 
-    response = client.get(PRE+'/bibs?format=mrk')
+    response = client.get(f'{PRE}/bibs?format=mrk')
     assert response.headers["Content-Type"] == 'text/plain; charset=utf-8'
     
-    response = client.get(PRE+'/bibs?format=mrc')
+    response = client.get(f'{PRE}/bibs?format=mrc')
     assert response.headers["Content-Type"] == 'application/marc'
     
-    response = client.get(PRE+'/bibs?format=txt')
+    response = client.get(f'{PRE}/bibs?format=txt')
     assert response.headers["Content-Type"] == 'text/plain; charset=utf-8'
     
-def test_search(client, records):
-    res = client.get(PRE+'/bibs?search={"900": {"a": "25"}}')
+def test_search(client):
+    res = client.get(f'{PRE}/bibs?search=' + '{"900": {"a": "25"}}')
     assert res.status_code == 200
     data = json.loads(res.data)
     assert len(data['results']) == 1
     
-    res = client.get(PRE+'/bibs?search={"900": {"a": "/^3\\\d/"}}')
+    res = client.get(f'{PRE}/bibs?search=' + '{"900": {"a": "/^3\\\d/"}}')
     assert res.status_code == 200
     data = json.loads(res.data)
     assert len(data['results']) == 10
     
-    res = client.get(PRE+'/auths?search={"OR": {"400": 0, "999": 1}}')
+    res = client.get(f'{PRE}/auths?search=' + '{"OR": {"400": 0, "999": 1}}')
     assert res.status_code == 200
     data = json.loads(res.data)
     assert len(data['results']) == 0
 
-def test_record(client,records):
-    data = json.loads(client.get(PRE+'/bibs/1').data)
+def test_record(client):
+    data = json.loads(client.get(f'{PRE}/bibs/1').data)
     assert data['result']['_id'] == 1
     
-    data = json.loads(client.get(PRE+'/auths/1').data)
+    data = json.loads(client.get(f'{PRE}/auths/1').data)
     assert data['result']['_id'] == 1
     
-def test_record_formats(client, records):
+def test_record_formats(client):
     for col in ('bibs', 'auths'):
         response = client.get('{}/{}/1?format=xml'.format(PRE, col))  
         assert response.headers["Content-Type"] == 'text/xml; charset=utf-8'
@@ -89,95 +91,130 @@ def test_record_formats(client, records):
         response = client.get('{}/{}/1?format=txt'.format(PRE, col))
         assert response.headers["Content-Type"] == 'text/plain; charset=utf-8'
 
-def test_records_fields_list(client, records):
-    data = json.loads(client.get(PRE+'/bibs/1/fields').data)
+def test_records_fields_list(client):
+    data = json.loads(client.get(f'{PRE}/bibs/1/fields').data)
     # this may change if future dlx version sets a 001 field automatically with the id
     assert len(data['results']) == 3
-    assert PRE+'/bibs/1/fields/245' in data['results']
-    assert PRE+'/bibs/1/fields/500' in data['results']
-    assert PRE+'/bibs/1/fields/900' in data['results']
+    assert f'{PRE}/bibs/1/fields/245' in data['results']
+    assert f'{PRE}/bibs/1/fields/500' in data['results']
+    assert f'{PRE}/bibs/1/fields/900' in data['results']
     
-    data = json.loads(client.get(PRE+'/auths/1/fields').data)
+    data = json.loads(client.get(f'{PRE}/auths/1/fields').data)
     # this may change if future dlx version sets a 001 field automatically with the id
     assert len(data['results']) == 3
-    assert PRE+'/auths/1/fields/100' in data['results']
-    assert PRE+'/auths/1/fields/400' in data['results']
-    assert PRE+'/auths/1/fields/900' in data['results']
+    assert f'{PRE}/auths/1/fields/100' in data['results']
+    assert f'{PRE}/auths/1/fields/400' in data['results']
+    assert f'{PRE}/auths/1/fields/900' in data['results']
     
-def test_record_field_place_list(client, records):
-    data = json.loads(client.get(PRE+'/bibs/1/fields/500').data)
+def test_record_field_place_list(client):
+    data = json.loads(client.get(f'{PRE}/bibs/1/fields/500').data)
     assert len(data['results']) == 2
-    assert PRE+'/bibs/1/fields/500/0' in data['results']
-    assert PRE+'/bibs/1/fields/500/1' in data['results']
+    assert f'{PRE}/bibs/1/fields/500/0' in data['results']
+    assert f'{PRE}/bibs/1/fields/500/1' in data['results']
     
-    data = json.loads(client.get(PRE+'/auths/1/fields/400').data)
+    data = json.loads(client.get(f'{PRE}/auths/1/fields/400').data)
     assert len(data['results']) == 2
-    assert PRE+'/auths/1/fields/400/0' in data['results']
-    assert PRE+'/auths/1/fields/400/1' in data['results']
+    assert f'{PRE}/auths/1/fields/400/0' in data['results']
+    assert f'{PRE}/auths/1/fields/400/1' in data['results']
     
-def test_record_field_place_subfield_list(client, records):
-    data = json.loads(client.get(PRE+'/bibs/1/fields/500/0').data)
+def test_record_field_place_subfield_list(client):
+    data = json.loads(client.get(f'{PRE}/bibs/1/fields/500/0').data)
     assert len(data['results']) == 1
-    assert PRE+'/bibs/1/fields/500/0/a' in data['results']
+    assert f'{PRE}/bibs/1/fields/500/0/a' in data['results']
     
-    data = json.loads(client.get(PRE+'/auths/1/fields/400/0').data)
+    data = json.loads(client.get(f'{PRE}/auths/1/fields/400/0').data)
     assert len(data['results']) == 1
-    assert PRE+'/auths/1/fields/400/0/a' in data['results']
+    assert f'{PRE}/auths/1/fields/400/0/a' in data['results']
     
-def test_record_field_place_subfield_place_list(client, records):
-    data = json.loads(client.get(PRE+'/bibs/1/fields/500/0/a').data)
+def test_record_field_place_subfield_place_list(client):
+    data = json.loads(client.get(f'{PRE}/bibs/1/fields/500/0/a').data)
     assert len(data['results']) == 1
-    data = json.loads(client.get(PRE+'/bibs/1/fields/500/1/a').data)
+    data = json.loads(client.get(f'{PRE}/bibs/1/fields/500/1/a').data)
     assert len(data['results']) == 2
-    assert PRE+'/bibs/1/fields/500/1/a/0' in data['results']
-    assert PRE+'/bibs/1/fields/500/1/a/1' in data['results']
+    assert f'{PRE}/bibs/1/fields/500/1/a/0' in data['results']
+    assert f'{PRE}/bibs/1/fields/500/1/a/1' in data['results']
     
-    data = json.loads(client.get(PRE+'/auths/1/fields/400/0/a').data)
+    data = json.loads(client.get(f'{PRE}/auths/1/fields/400/0/a').data)
     assert len(data['results']) == 1
-    data = json.loads(client.get(PRE+'/auths/1/fields/400/1/a').data)
+    data = json.loads(client.get(f'{PRE}/auths/1/fields/400/1/a').data)
     assert len(data['results']) == 2
-    assert PRE+'/auths/1/fields/400/1/a/0' in data['results']
-    assert PRE+'/auths/1/fields/400/1/a/1' in data['results']
+    assert f'{PRE}/auths/1/fields/400/1/a/0' in data['results']
+    assert f'{PRE}/auths/1/fields/400/1/a/1' in data['results']
     
-def test_record_field_place_subfield_place(client, records):
-    data = json.loads(client.get(PRE+'/bibs/1/fields/500/1/a/1').data)
+def test_record_field_place_subfield_place(client):
+    data = json.loads(client.get(f'{PRE}/bibs/1/fields/500/1/a/1').data)
     assert data['result'] == '3x'
     
-    data = json.loads(client.get(PRE+'/auths/1/fields/400/1/a/1').data)
+    data = json.loads(client.get(f'{PRE}/auths/1/fields/400/1/a/1').data)
     assert data['result'] == '3x'
     
-def test_record_field_subfields_list(client, records):
-    data = json.loads(client.get(PRE+'/bibs/1/subfields').data)
+def test_record_field_subfields_list(client):
+    data = json.loads(client.get(f'{PRE}/bibs/1/subfields').data)
     assert len(data['results']) == 9
-    assert PRE+'/bibs/1/fields/245/0/a/0' in data['results']
-    assert PRE+'/bibs/1/fields/500/1/a/0' in data['results']
-    assert PRE+'/bibs/1/fields/500/1/a/1' in data['results']
+    assert f'{PRE}/bibs/1/fields/245/0/a/0' in data['results']
+    assert f'{PRE}/bibs/1/fields/500/1/a/0' in data['results']
+    assert f'{PRE}/bibs/1/fields/500/1/a/1' in data['results']
     
-def test_create_record(client, records):
+def test_create_record(client):
     data = '{"_id": 1, "invalid": 1}'
-    response = client.post(PRE+'/bibs', headers={}, data=json.dumps(data))
+    response = client.post(f'{PRE}/bibs', headers={}, data=json.dumps(data))
     assert response.status_code == 400
     
-    data = {"245": [{"indicators": [" ", " "], "subfields": [{"code": "a", "value": "A new record"}]}]}    
-    response = client.post(PRE+'/bibs', headers={}, data=json.dumps(data))
+    data = {"245": [{"indicators": [" ", " "], "subfields": [{"code": "a", "value": "A new record"}]}]}
+    response = client.post(f'{PRE}/bibs', headers={}, data=json.dumps(data))
     assert response.status_code == 200
-    assert client.get(PRE+'/bibs/51').status_code == 200
-
-def test_delete_record(client, records):
-    assert client.delete(PRE+'/bibs/1').status_code == 200
-    assert client.get(PRE+'/bibs/1').status_code == 404
+    assert client.get(f'{PRE}/bibs/51').status_code == 200
     
-def test_update_record(client, records):
+def test_create_record_from_mrk(client):
+    data = 'invalid'
+    response = client.post(f'{PRE}/bibs', headers={}, data=data)
+    assert response.status_code == 400
+    
+    data = '=000  leader\n=245  \\\\$aYet another title$bsubtitle\n=269  \\\\$a2020'
+    response = client.post(f'{PRE}/bibs?format=mrk', headers={}, data=data)
+    assert response.status_code == 200
+    
+    response = client.get(f'{PRE}/bibs/52')
+    assert json.loads(response.data)['result'] == \
+        {
+            "_id": 52,
+            "000": ['leader'],
+            "245": [{"indicators": [" ", " "], "subfields": [{"code": "a", "value": "Yet another title"}, {'code': 'b', 'value': 'subtitle'}]}],
+            "269": [{"indicators": [" ", " "], "subfields": [{"code": "a", "value": "2020"}]}]
+        } 
+    
+def test_delete_record(client):
+    assert client.delete(f'{PRE}/bibs/1').status_code == 200
+    assert client.get(f'{PRE}/bibs/1').status_code == 404
+    
+def test_update_record(client):
     data = '{"_id": 1, "invalid": 1}'
-    response = client.put(PRE+'/bibs/1', headers={}, data=json.dumps(data))
+    response = client.put(f'{PRE}/bibs/1', headers={}, data=json.dumps(data))
     assert response.status_code == 400
 
     data = {"_id": 1, "245": [{"indicators": [" ", " "], "subfields": [{"code": "a", "value": "An updated title"}]}]}    
-    response = client.put(PRE+'/bibs/1', headers={}, data=json.dumps(data))
+    response = client.put(f'{PRE}/bibs/1', headers={}, data=json.dumps(data))
     assert response.status_code == 200
     
-    data = json.loads(client.get(PRE+'/bibs/1/fields/245/0/a/0').data)
+    data = json.loads(client.get(f'{PRE}/bibs/1/fields/245/0/a/0').data)
     assert data['result'] == "An updated title"
-    assert client.get(PRE+'/bibs/1/fields/500/0/a/0').status_code == 404
+    assert client.get(f'{PRE}/bibs/1/fields/500/0/a/0').status_code == 404
     
+def test_update_record_from_mrk(client):
+    data = 'invalid'
+    response = client.put(f'{PRE}/bibs/1', headers={}, data=data)
+    assert response.status_code == 400
+    
+    data = '=000  leader\n=245  \\\\$aYet another title$bsubtitle\n=269  \\\\$a2020'
+    response = client.put(f'{PRE}/bibs/25?format=mrk', headers={}, data=data)
+    assert response.status_code == 200
+    
+    response = client.get(f'{PRE}/bibs/25')
+    assert json.loads(response.data)['result'] == \
+        {
+            "_id": 25,
+            "000": ['leader'],
+            "245": [{"indicators": [" ", " "], "subfields": [{"code": "a", "value": "Yet another title"}, {'code': 'b', 'value': 'subtitle'}]}],
+            "269": [{"indicators": [" ", " "], "subfields": [{"code": "a", "value": "2020"}]}]
+        }
     
