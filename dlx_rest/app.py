86899a2dcc9e8550d9768c9c16d416a3cce266ef
Aaron Helton <mariusagricola@gmail.com>
2021-02-24 14:14:54 -0500

    move db connects to app.py

--- dlx_rest/app.py
+++ dlx_rest/app.py
@@ -2,6 +2,7 @@ from flask import Flask, Response, url_for, jsonify, abort as flask_abort
 from flask_restx import Resource, Api, reqparse
 from flask_login import LoginManager
 from pymongo import ASCENDING as ASC, DESCENDING as DESC
+from mongoengine import connect, disconnect
 from flask_cors import CORS
 from dlx import DB
 from dlx.marc import BibSet, Bib, AuthSet, Auth
@@ -16,6 +17,9 @@ login_manager = LoginManager()
 login_manager.init_app(app)
 login_manager.login_view = "login"
 login_manager.login_message =""
+
+connect(host=Config.connect_string,db=Config.dbname)
+DB.connect(Config.connect_string)
 
 try:
     app.secret_key=Config.secret_key
