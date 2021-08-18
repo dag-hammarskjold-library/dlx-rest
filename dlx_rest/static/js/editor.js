3a115e3e3ef3aa45143f0b804131950018e444ec
Eric Ade ATTERE NGOUA <ericadeatterengoua@erics-mbp.myfiosgateway.com>
2021-08-18 09:21:53 -0400

    merge implementation in progress

--- dlx_rest/static/js/editor.js
+++ dlx_rest/static/js/editor.js
@@ -10,19 +10,19 @@ import { multiplemarcrecordcomponent } from "./record.js";
 import { messagecomponent } from "./messagebar.js";
 import { modalmergecomponent } from "./merge.js";
 
-
 /////////////////////////////////////////////////////////////////
 // VIEW MODEL DEFINITION
 /////////////////////////////////////////////////////////////////
-let vm_new_ui_component = new Vue({
+export let vm_new_ui_component = new Vue({
     el: '#new_ui_component',
-     components: { headercomponent, basketcomponent, warningcomponent, multiplemarcrecordcomponent, messagecomponent, modalmergecomponent },
+    components: { headercomponent, basketcomponent, warningcomponent, multiplemarcrecordcomponent, messagecomponent , modalmergecomponent},
     data: {
       visible: false,
       recordToDisplay: "",
       recordDisplayed: [],
       maxRecordToDisplay: 26,
-      records: []
+      records: [],
+      showModal:false
     },
     methods: {}
   })
