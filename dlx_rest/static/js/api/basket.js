fab6e68bf5cbdf9bb050424f9ac25b895ba1ef82
Aaron Helton <mariusagricola@gmail.com>
2021-10-01 15:46:40 -0400

    Send to editor (#264)
    * setup send to editor
    
    * fix edit icon
    
    * re-add style

--- dlx_rest/static/js/api/basket.js
+++ dlx_rest/static/js/api/basket.js
@@ -3,11 +3,12 @@ import { Jmarc } from "../jmarc.js";
 export default {
     // Individual item methods
     async createItem(api_prefix, basket_id='userprofile/my_profile/basket', collection, record_id) {
+        
         Jmarc.apiUrl = api_prefix;
         let url = `${api_prefix}${basket_id}`;
         let myItemTitle = "";
         let myId = null;
-        await Jmarc.get(collection, record_id).then(jmarc => {
+        await Jmarc.get(collection, record_id).then(async jmarc => {
             if(collection == "bibs") {
                 let myTitleField = jmarc.getField(245,0);
                 let myTitle = [];
@@ -16,6 +17,7 @@ export default {
                 }
                 myItemTitle = myTitle.join(" ");
             } else if (collection == "auths") {
+                console.log("Trying to create an auth basket item...")
                 let myTitleField = jmarc.fields.filter(x => x.tag.match(/^1[0-9][0-9]/))[0];
                 let myTitle = [];
                 for (let s in myTitleField.subfields) {
@@ -24,12 +26,14 @@ export default {
                 myItemTitle = myTitle.join(" ");
             }
             let data = `{"collection": "${collection}", "record_id": "${record_id}", "title": "${myItemTitle}"}`
-            fetch(url, {
+            await fetch(url, {
                 method: 'POST',
                 body: data
+            }).then( () => {
+                console.log("and we did")
+                return true;
             });
         });
-        return true;
     },
     async getItem(api_prefix, collection, record_id) {
         Jmarc.api_prefix = api_prefix;
@@ -58,5 +62,13 @@ export default {
     },
     clearItems(api_prefix, basket_id='userprofile/my_profile/basket') {
         let url = `${api_prefix}/${basket_id}`
+    },
+    contains(collection, record_id, basket) {
+        for (let item of basket) {
+            if (item.collection == collection && item.record_id == record_id) {
+                return true;
+            }
+        }
+        return false;
     }
 }
