4ec457b8551d1b9841ae558f785fb72a858ae9e1
aaronhelton <helton@un.org>
2021-11-04 15:29:09 -0400

    display results

--- dlx_rest/static/js/search/browse.js
+++ dlx_rest/static/js/search/browse.js
@@ -20,9 +20,29 @@ export let browsecomponent = {
     template: `
     <div class="col-sm-8 pt-2" id="app1" style="background-color:white;">
         <div class="row"></div>
-        <div v-for="result in results_before" class="row">{{result}}</div>
-        <div class="row">{{search_term}}</div>
-        <div v-for="result in results_after" class="row">{{result}}</div>
+        <div class="row">
+            <div id="before-spinner" class="col d-flex justify-content-center">
+                <div class="spinner-border" role="status">
+                    <span class="sr-only">Loading...</span>
+                </div>
+            </div>
+        </div>
+        <div v-for="result in results_before" class="row">
+            <div class="col">{{result.value.join(', ')}}</div>
+        </div>
+        <div class="row">
+            <div class="col"><i class="fas fa-angle-double-right mr-2 text-success"></i><span class="text-success">{{q}}</span></div>
+        </div>
+        <div class="row">
+            <div id="after-spinner" class="col d-flex justify-content-center">
+                <div class="spinner-border" role="status">
+                    <span class="sr-only">Loading...</span>
+                </div>
+            </div>
+        </div>
+        <div v-for="result in results_after" class="row">
+            <div class="col">{{result.value.join(', ')}}</div>
+        </div>
     </div>`,
     data: function () {
         return {
@@ -31,13 +51,24 @@ export let browsecomponent = {
             search_term: null
         }
     },
-    created: async function () {
+    mounted: async function () {
         let beforeBrowse = `${this.api_prefix}marc/${this.collection}/records/browse?search=${this.index}:${this.q}&compare=less`
         let afterBrowse = `${this.api_prefix}marc/${this.collection}/records/browse?search=${this.index}:${this.q}&compare=greater`
         fetch(beforeBrowse).then(response => {
-            response.json().then(data => {
-                console.log(data)
+            response.json().then(jsondata => {
+                this.results_before = jsondata.data
+            }).then( () => {
+                let spinner = document.getElementById('before-spinner')
+                spinner.remove()
             })
+        })
+        fetch(afterBrowse).then(response => {
+            response.json().then(jsondata => {
+                this.results_after = jsondata.data
+            })
+        }).then( () => {
+            let spinner = document.getElementById('after-spinner')
+            spinner.remove()
         })
     }
 }
