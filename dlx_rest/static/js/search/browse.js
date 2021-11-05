2ad93ae3bfd5374bbd405ecc4b188f2a889286bf
aaronhelton <helton@un.org>
2021-11-04 20:13:35 -0400

    add links

--- dlx_rest/static/js/search/browse.js
+++ dlx_rest/static/js/search/browse.js
@@ -19,7 +19,14 @@ export let browsecomponent = {
     },
     template: `
     <div class="col-sm-8 pt-2" id="app1" style="background-color:white;">
-        <div class="row"></div>
+        <nav>
+            <ul class="pagination pagination-md justify-content-center">
+                <li v-if="prev" class="page-item"><a class="page-link" :href="prev">Previous</a></li>
+                <li v-else class="page-item disabled"><a class="page-link" href="">Previous</a></li>
+                <li v-if="next" class="page-item"><a class="page-link" :href="next">Next</a></li>
+                <li v-else class="page-item disabled"><a class="page-link" href="">Next</a></li>
+            </ul>
+        </nav>
         <div class="row">
             <div id="before-spinner" class="col d-flex justify-content-center">
                 <div class="spinner-border" role="status">
@@ -27,8 +34,8 @@ export let browsecomponent = {
                 </div>
             </div>
         </div>
-        <div v-for="result in results_before" class="row">
-            <div class="col">{{result.value.join(', ')}}</div>
+        <div v-for="result in results_before" class="row my-2">
+            <div class="col"><a :href="result.url" target="_blank">{{result.value}}</a></div>
         </div>
         <div class="row">
             <div class="col"><i class="fas fa-angle-double-right mr-2 text-success"></i><span class="text-success">{{q}}</span></div>
@@ -40,15 +47,17 @@ export let browsecomponent = {
                 </div>
             </div>
         </div>
-        <div v-for="result in results_after" class="row">
-            <div class="col">{{result.value.join(', ')}}</div>
+        <div v-for="result in results_after" class="row my-2">
+            <div class="col"><a :href="result.url" target="_blank">{{result.value}}</a></div>
         </div>
     </div>`,
     data: function () {
         return {
             results_before: [],
             results_after: [],
-            search_term: null
+            search_term: null,
+            next: null,
+            prev: null
         }
     },
     mounted: async function () {
@@ -56,7 +65,14 @@ export let browsecomponent = {
         let afterBrowse = `${this.api_prefix}marc/${this.collection}/records/browse?search=${this.index}:${this.q}&compare=greater`
         fetch(beforeBrowse).then(response => {
             response.json().then(jsondata => {
-                this.results_before = jsondata.data
+                //this.results_before = jsondata.data
+                for (let result of jsondata.data) {
+                    let myRecordId = result.url.split('/').pop()
+                    let myUrl = `${this.api_prefix.replace("/api", "")}records/${this.collection}/${myRecordId}`
+                    let myValues = result.value.join(" | ")
+                    this.results_before.push({'url': myUrl, 'value': myValues})
+                }
+                this.prev = jsondata._links._next
             }).then( () => {
                 let spinner = document.getElementById('before-spinner')
                 spinner.remove()
@@ -64,7 +80,13 @@ export let browsecomponent = {
         })
         fetch(afterBrowse).then(response => {
             response.json().then(jsondata => {
-                this.results_after = jsondata.data
+                this.next = jsondata._links._next
+                for (let result of jsondata.data) {
+                    let myRecordId = result.url.split('/').pop()
+                    let myUrl = `${this.api_prefix.replace("/api", "")}records/${this.collection}/${myRecordId}`
+                    let myValues = result.value.join(" | ")
+                    this.results_after.push({'url': myUrl, 'value': myValues})
+                }
             })
         }).then( () => {
             let spinner = document.getElementById('after-spinner')
