104011f7a77ec36843400d8b7790395c2b4f893a
aaronhelton <helton@un.org>
2021-11-12 09:05:21 -0500

    tweak deploy

--- deploy.sh
+++ deploy.sh
@@ -1,10 +1,16 @@
 # Use this script to deploy to one of the four deployment environments we have
 # configured: dev, qat, uat, and prod
-echo "Deploying to $1 environment"
+ENV=$1
+
+if [ -z $1 ]
+then
+	ENV="dev"
+fi
+echo "Deploying to $ENV environment"
 
 PWD=`pwd`
 
 git pull
 . $PWD/venv/bin/activate
 pip install --upgrade git+https://github.com/dag-hammarskjold-library/dlx#egg=dlx
-zappa update $1
+zappa update $ENV
