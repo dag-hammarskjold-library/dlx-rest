0818cf28ec0c8a5a04feb5bf8291f12b712ce6a9
aaronhelton <helton@un.org>
2021-11-12 08:17:48 -0500

    create activate script

---
+++ activate.sh
@@ -0,0 +1,22 @@
+# Use this script to activate  one of the four deployment environments we have
+# configured: DEV, QAT, UAT, and PROD
+
+ENV=$1
+
+if [ -z $1 ]
+then
+	ENV="DEV"
+fi
+
+echo "Activating $ENV environment"
+
+PWD=`pwd`
+
+. $PWD/venv/bin/activate
+unset DLX_REST_
+unset DLX_REST_DEV
+unset DLX_REST_QAT
+unset DLX_REST_UAT
+unset DLX_REST_PROD
+export FLASK_APP="dlx_rest.app"
+export DLX_REST_$ENV="True"
