0d182fdd1d69bfc0c6299fde1f78dd78ab9973cb
Aaron Helton <mariusagricola@gmail.com>
2022-01-07 10:30:24 -0500

    Reverse basket (#342)
    * reverse basket order
    
    * set basket update interval
    
    * space time out and add refresh icon
    
    * order basket items by recency (desc)

--- dlx_rest/static/js/api/basket.js
+++ dlx_rest/static/js/api/basket.js
@@ -63,7 +63,7 @@ export default {
         let url = `${api_prefix}${basket_id}`
         const response = await fetch(url);
         const jsonData = await response.json();
-        return jsonData.data.item_data;
+        return jsonData.data.item_data.sort((a,b) => a - b);
     },
     clearItems(api_prefix, basket_id='userprofile/my_profile/basket') {
         let url = `${api_prefix}/${basket_id}`
