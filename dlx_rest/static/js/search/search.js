80d862c9edcc6db7e3f7aad393493d7c09e3532c
aaronhelton <helton@un.org>
2022-04-20 15:21:24 -0400

    move search code to mounted hook

--- dlx_rest/static/js/search/search.js
+++ dlx_rest/static/js/search/search.js
@@ -132,6 +132,9 @@ export let searchcomponent = {
         }
     },
     created: async function() {
+        
+    },
+    mounted: async function() {
         let component = this;
         
         // [what is this used for?]
@@ -228,59 +231,68 @@ export let searchcomponent = {
                     this.reportError(error.toString())
                 }
             }
-        );
-        
-        // cancel the search if it takes more than 15 seconds
-        setTimeout(() => this.abortController.abort(), this.maxTime);
-    },
-    mounted: async function() {
-        user.getProfile(this.api_prefix, 'my_profile').then(
-            myProfile => {
-                if (myProfile) {
-                    this.user = myProfile.data.email;
-                }
-
-                if (typeof this.user !== "undefined") {
-                    basket.getBasket(this.api_prefix).then(
-                        myBasket => {
-                            for (let result of this.results) {
-                                let myId = `icon-${this.collection}-${result._id}`
-                                let iconEl = document.getElementById(myId);
-                
-                                iconEl.classList.add('fa-folder-plus');
-                                iconEl.addEventListener("click", async () => {
-                                    basket.getBasket(this.api_prefix).then(
-                                        myBasket => {
-                                            this.toggleAddRemove(iconEl, myBasket, this.collection, result._id);
-                
-                                            if (this.basketContains(myBasket, this.collection, result._id)) {
+        ).then( () => {
+            user.getProfile(this.api_prefix, 'my_profile').then(
+                myProfile => {
+                    console.log("got my profile")
+                    if (myProfile) {
+                        this.user = myProfile.data.email;
+                    }
+    
+                    if (typeof this.user !== "undefined") {
+                        console.log("this user is not undefined")
+                        basket.getBasket(this.api_prefix).then(
+                            myBasket => {
+                                console.log("got my basket contents")
+                                if (this.results.length == 0) {
+                                    console.log("Zero length results? How?")
+                                }
+                                for (let result of this.results) {
+                                    console.log("processing result")
+                                    let myId = `icon-${this.collection}-${result._id}`
+                                    let iconEl = document.getElementById(myId);
+                    
+                                    iconEl.classList.add('fa-folder-plus');
+                                    iconEl.addEventListener("click", async () => {
+                                        basket.getBasket(this.api_prefix).then(
+                                            myBasket => {
+                                                this.toggleAddRemove(iconEl, myBasket, this.collection, result._id);
+                    
+                                                if (this.basketContains(myBasket, this.collection, result._id)) {
+                                                    iconEl.classList.remove('fa-folder-plus',);
+                                                    iconEl.classList.add('fa-folder-minus');
+                                                    iconEl.title = "Remove from basket";
+                                                }
+                                            }
+                                        );
+                                    });
+                    
+                                    // checking if the record is locked and displaying a lock if it is.
+                                    basket.itemLocked(this.api_prefix, this.collection, result._id).then(
+                                        itemLocked => {
+                                            if (itemLocked["locked"] == true && itemLocked["by"] != this.user) {
+                                                // Display a lock icon
                                                 iconEl.classList.remove('fa-folder-plus',);
-                                                iconEl.classList.add('fa-folder-minus');
-                                                iconEl.title = "Remove from basket";
+                                                iconEl.classList.remove('fa-folder-minus',);
+                                                iconEl.classList.add('fa-lock',); // To do: add a click event here to "unlock" the item
+                                                iconEl.title = `This item is locked by ${itemLocked["by"]}`;
                                             }
                                         }
-                                    );
-                                });
-                
-                                // checking if the record is locked and displaying a lock if it is.
-                                basket.itemLocked(this.api_prefix, this.collection, result._id).then(
-                                    itemLocked => {
-                                        if (itemLocked["locked"] == true && itemLocked["by"] != this.user) {
-                                            // Display a lock icon
-                                            iconEl.classList.remove('fa-folder-plus',);
-                                            iconEl.classList.remove('fa-folder-minus',);
-                                            iconEl.classList.add('fa-lock',); // To do: add a click event here to "unlock" the item
-                                            iconEl.title = `This item is locked by ${itemLocked["by"]}`;
-                                        }
-                                    }
-                                )
+                                    )
+                                }
                             }
-                        }
-                    )
+                        )
+                    }
+    
                 }
+            )
+        })
+        
+        
 
-            }
-        )
+
+        // cancel the search if it takes more than 15 seconds
+        setTimeout(() => this.abortController.abort(), this.maxTime);
     },
     methods: {
         async getMyBasket(url) {
