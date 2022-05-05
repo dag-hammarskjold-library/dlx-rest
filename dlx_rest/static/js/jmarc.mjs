2199e33a3c21c0ba9253ba84a8ccb243975fe052
jbukhari <joe.bukhari@gmail.com>
2022-05-05 12:01:29 -0400

    fix history method; update history tests (#425)
    * fix history method; update history tests
    
    * diff method
    
    Co-authored-by: jbukhari <jb@Js-MacBook-Air.local>

--- dlx_rest/static/js/jmarc.mjs
+++ dlx_rest/static/js/jmarc.mjs
@@ -342,7 +342,7 @@ export class Jmarc {
     }
     
     static fromWorkform(collection, workformName) {
-        let jmarc = new Jmarc(collection);
+        let jmarc = new Workform(collection);
         
         return fetch(jmarc.collectionUrl + '/workforms/' + workformName).then(
             response => {
@@ -547,6 +547,7 @@ export class Jmarc {
 	parse(data={}) {
 		this.updated = data['updated'];
 		this.user = data['user'];
+		this.fields = [];
 		
 		let tags = Object.keys(data).filter(x => x.match(/^\d{3}/));
 		tags = tags.sort((a, b) => parseInt(a) - parseInt(b));
@@ -610,15 +611,33 @@ export class Jmarc {
 		let data = json['data'];
 		let historyRecords = [];
 		
-		for (let url of data) {
+		for (let result of data) {
 			let record = new Jmarc(this.collection);
-			let response = await fetch(url);
+			let response = await fetch(result.event);
 			let json = await response.json();
 			record.parse(json['data']);
 			historyRecords.push(record);
 		}
 		
 		return historyRecords
+	}
+
+	diff(other) {
+		// returns a new Jmarc record where the fields different from "other" are tagged
+		if (! other instanceof Jmarc) {throw new Error("First argument must be instance of Jmarc")};
+
+		let diff = new Diff(this.collection);
+		diff.parse(this.compile());
+
+		for (let field of diff.fields) {
+			if (other.fields.map(x => x.toStr()).includes(field.toStr())) {
+				field.isDiff = false
+			} else {
+				field.isDiff = true
+			}
+		}
+
+		return diff
 	}
 
 	clone() {
@@ -802,6 +821,13 @@ export class Workform extends Jmarc {
     
     constructor(collection, name=null) {
 		super(collection);
-        
+	}
+}
+
+export class Diff extends Jmarc {
+	// work in progress
+    
+	constructor(collection) {
+		super(collection);
 	}
 }
