ab9a3b422fe4c585814d4cc2375ac3fac02a0920
jbukhari <joe.bukhari@gmail.com>
2022-07-15 09:50:02 -0400

    Bugfix20220714 (#499)
    * auth creation button bugfix
    
    * pending icon for auth create button

--- dlx_rest/static/js/record.js
+++ dlx_rest/static/js/record.js
@@ -2554,6 +2554,11 @@ function keyupAuthLookup(event) {
     addButton.className = "fas fa-solid fa-plus float-left mr-2 create-authority";
 
     addButton.addEventListener("click", function() {
+        subfield.xrefCell.innerHTML = null;
+        let spinner = document.createElement("i");
+        spinner.className = "fa fa-spinner";
+        subfield.xrefCell.append(spinner);
+
         // create and save the new authority record
         let tag = jmarc.authMap[field.tag][subfield.code];
         let auth = new Jmarc("auths");
@@ -2561,10 +2566,11 @@ function keyupAuthLookup(event) {
         
         auth.post().then(
             auth => {
+                subfield.xref = auth.recordId;
                 subfield.valueSpan.classList.remove("authority-controlled-unmatched");
                 subfield.xrefCell.innerHTML = null;
 
-                // create the xerf link
+                // create the xref link
                 let xrefLink = document.createElement("a");
                 subfield.xrefCell.appendChild(xrefLink);
                 xrefLink.href = component.baseUrl + `records/auths/${auth.recordId}`;
