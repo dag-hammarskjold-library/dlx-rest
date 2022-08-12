865dd46549c9ab638f5b31c0b9d314e9f6891d60
Aaron Helton <helton@un.org>
2022-08-12 15:27:43 -0400

    attempt auth delete before removing record from basket (#561)
    * attempt auth delete before removing record from basket
    
    * reload basket after record delete

--- dlx_rest/static/js/record.js
+++ dlx_rest/static/js/record.js
@@ -583,14 +583,14 @@ export let multiplemarcrecordcomponent = {
                     let deletedRid = jmarc.recordId;
                     let deletedColl = jmarc.collection;
  
-                    this.$root.$refs.basketcomponent.removeRecordFromList(jmarc.collection, jmarc.recordId).then( () => {
-                        jmarc.delete().then( () => {
-                            this.removeRecordFromEditor(jmarc);
-                            this.callChangeStyling(`Record ${deletedColl}/${deletedRid} has been deleted`, "row alert alert-success");
-                        }).catch( error => {
-                            this.callChangeStyling(error.message,"row alert alert-danger");
-                        });
-                    })
+                    jmarc.delete().then( () => {
+                        this.removeRecordFromEditor(jmarc);
+                        this.callChangeStyling(`Record ${deletedColl}/${deletedRid} has been deleted`, "row alert alert-success");
+                        //this.$root.$refs.basketcomponent.removeRecordFromList(jmarc.collection, jmarc.recordId)
+                        this.$root.$refs.basketcomponent.rebuildBasket()
+                    }).catch( error => {
+                        this.callChangeStyling(error.message,"row alert alert-danger");
+                    });
                 }
             }
  
