fe68280182ad080f3ca068dd794dda4169e37424
jbukhari <jb@Js-MacBook-Air.local>
2022-08-17 09:58:46 -0400

    remove delay when replacing open record

--- dlx_rest/static/js/record.js
+++ dlx_rest/static/js/record.js
@@ -235,6 +235,8 @@ export let multiplemarcrecordcomponent = {
                         } else {
                             basket.createItem(this.prefix, "userprofile/my_profile/basket", jmarc.collection, jmarc.recordId).then( () => {
                                 this.$root.$refs.basketcomponent.rebuildBasket()
+                                
+                                // wait for basket to display record so the display method can update the basket styling 
                                 this.displayMarcRecord(jmarc);
                             })
 
