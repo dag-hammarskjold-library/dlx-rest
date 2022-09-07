24ab87a88bba7b8353270e8a6d160d21b28abce7
aaronhelton <helton@un.org>
2022-09-07 15:06:48 -0400

    lengthen display name in model

--- dlx_rest/models.py
+++ dlx_rest/models.py
@@ -67,7 +67,7 @@ class RecordView(Document):
 
 class User(UserMixin, Document):
     email = StringField(max_length=200, required=True, unique=True)
-    display = StringField(max_length=10, required=True)
+    display = StringField(max_length=200, required=True)
     password_hash = StringField(max_length=200)
     roles = ListField(ReferenceField(Role))
     default_views = ListField(ReferenceField(RecordView))
