513fcfe6fb36e1a6062b3a53c3c8fe8708dcd21b
aaronhelton <helton@un.org>
2022-09-26 15:24:08 -0400

    preserve search parameters in simple search

--- dlx_rest/static/js/search/search.js
+++ dlx_rest/static/js/search/search.js
@@ -110,6 +110,7 @@ export let searchcomponent = {
             <form class="form-inline mr-auto col-lg-12" :action="action">
                 <input v-if="params.search" id="q" name="q" class="form-control mr-sm-2 col-lg-10" type="search" :aria-label="'Search ' + collection + ' collection'" :value="params.search">
                 <input v-else id="q" name="q" class="form-control mr-sm-2 col-lg-10" type="search" :placeholder="'Search ' + collection + ' collection'" aria-label="Search this collection">
+                <input v-for="v,k in params" type="hidden" :id="k" :name="k" :value="v">
                 <button class="btn btn-primary" type="submit" id="search-btn" value="Search">Search</button>
                 <button class="btn btn-sm btn-default" type="button" value="Cancel search" title="Cancel" v-on:click="cancelSearch()">
                     <span>X</span>
