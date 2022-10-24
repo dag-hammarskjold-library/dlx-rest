08f2be56279bedd1f3fcb028d926971b33c8816d
jbukhari <jb@Js-MacBook-Air.local>
2022-10-24 14:57:19 -0400

    update bib 049 and 089

--- dlx_rest/static/js/validation.js
+++ dlx_rest/static/js/validation.js
@@ -74,7 +74,7 @@ export const validationData = {
             "validIndicators2": ["*"],
             "requiredSubfields": [],
             "validSubfields": ["*"],
-            "defaultSubfields": ["a"] 
+            "defaultSubfields": ["a"]
         },
         "035": {
             "name": "",
@@ -167,12 +167,260 @@ export const validationData = {
             "validIndicators2": [],
             "requiredSubfields": ["a"],
             "validSubfields": ["*"],
-            "defaultSubfields": ["a"]
-            /* To do: get a list of ISO Alpha3 codes
+            "defaultSubfields": ["a"],
             "validStrings": {
-                "a": []
+                "a": [
+                    "abw", // Aruba
+                    "afg", // Afghanistan
+                    "ago", // Angola
+                    "aia", // Anguilla
+                    "ala", // Åland Islands
+                    "aLB", // Albania
+                    "aND", // Andorra
+                    "are", // United Arab Emirates
+                    "arg", // Argentina
+                    "arm", // Armenia
+                    "asm", // American Samoa
+                    "ata", // Antarctica
+                    "atf", // French Southern Territories
+                    "atg", // Antigua and Barbuda
+                    "aus", // Australia
+                    "aut", // Austria
+                    "aze", // Azerbaijan
+                    "bdi", // Burundi
+                    "bel", // Belgium
+                    "ben", // Benin
+                    "bes", // Bonaire, Sint Eustatius and Saba
+                    "bfa", // Burkina Faso
+                    "bgd", // Bangladesh
+                    "bgr", // Bulgaria
+                    "bhr", // Bahrain
+                    "bhs", // Bahamas
+                    "bih", // Bosnia and Herzegovina
+                    "blm", // Saint Barthélemy
+                    "blr", // Belarus
+                    "blz", // Belize
+                    "bmu", // Bermuda
+                    "bol", // Bolivia (Plurinational State of)
+                    "bra", // Brazil
+                    "brb", // Barbados
+                    "brn", // Brunei Darussalam
+                    "btn", // Bhutan
+                    "bvt", // Bouvet Island
+                    "bwa", // Botswana
+                    "caf", // Central African Republic
+                    "can", // Canada
+                    "cck", // Cocos (Keeling) Islands
+                    "che", // Switzerland
+                    "chl", // Chile
+                    "chn", // China
+                    "civ", // Côte d'Ivoire
+                    "cmr", // Cameroon
+                    "cod", // Congo, Democratic Republic of the
+                    "cog", // Congo
+                    "cok", // Cook Islands
+                    "col", // Colombia
+                    "com", // Comoros
+                    "cpv", // Cabo Verde
+                    "cri", // Costa Rica
+                    "cub", // Cuba
+                    "cuw", // Curaçao
+                    "cxr", // Christmas Island
+                    "cym", // Cayman Islands
+                    "cyp", // Cyprus
+                    "cze", // Czechia
+                    "deu", // Germany
+                    "dji", // Djibouti
+                    "dma", // Dominica
+                    "dnk", // Denmark
+                    "dom", // Dominican Republic
+                    "dza", // Algeria
+                    "ecu", // Ecuador
+                    "egy", // Egypt
+                    "eri", // Eritrea
+                    "esh", // Western Sahara
+                    "esp", // Spain
+                    "est", // Estonia
+                    "eth", // Ethiopia
+                    "fin", // Finland
+                    "fji", // Fiji
+                    "flk", // Falkland Islands (Malvinas)
+                    "fra", // France
+                    "fro", // Faroe Islands
+                    "fsm", // Micronesia (Federated States of)
+                    "gab", // Gabon
+                    "gbr", // United Kingdom of Great Britain and Northern Ireland
+                    "geo", // Georgia
+                    "ggy", // Guernsey
+                    "gha", // Ghana
+                    "gib", // Gibraltar
+                    "gin", // Guinea
+                    "glp", // Guadeloupe
+                    "gmb", // Gambia
+                    "gnb", // Guinea-Bissau
+                    "gnq", // Equatorial Guinea
+                    "grc", // Greece
+                    "grd", // Grenada
+                    "grl", // Greenland
+                    "gtm", // Guatemala
+                    "guf", // French Guiana
+                    "gum", // Guam
+                    "guy", // Guyana
+                    "hkg", // Hong Kong
+                    "hmd", // Heard Island and McDonald Islands
+                    "hnd", // Honduras
+                    "hrv", // Croatia
+                    "hti", // Haiti
+                    "hun", // Hungary
+                    "idn", // Indonesia
+                    "imn", // Isle of Man
+                    "ind", // India
+                    "iot", // British Indian Ocean Territory
+                    "irl", // Ireland
+                    "irn", // Iran (Islamic Republic of)
+                    "irq", // Iraq
+                    "isl", // Iceland
+                    "isr", // Israel
+                    "ita", // Italy
+                    "jam", // Jamaica
+                    "jey", // Jersey
+                    "jor", // Jordan
+                    "jpn", // Japan
+                    "kaz", // Kazakhstan
+                    "ken", // Kenya
+                    "kgz", // Kyrgyzstan
+                    "khm", // Cambodia
+                    "kir", // Kiribati
+                    "kna", // Saint Kitts and Nevis
+                    "kor", // Korea, Republic of
+                    "kwt", // Kuwait
+                    "lao", // Lao People's Democratic Republic
+                    "lbn", // Lebanon
+                    "lbr", // Liberia
+                    "lby", // Libya
+                    "lca", // Saint Lucia
+                    "lie", // Liechtenstein
+                    "lka", // Sri Lanka
+                    "lso", // Lesotho
+                    "ltu", // Lithuania
+                    "lux", // Luxembourg
+                    "lva", // Latvia
+                    "mac", // Macao
+                    "maf", // Saint Martin (French part)
+                    "mar", // Morocco
+                    "mco", // Monaco
+                    "mda", // Moldova, Republic of
+                    "mdg", // Madagascar
+                    "mdv", // Maldives
+                    "mex", // Mexico
+                    "mhl", // Marshall Islands
+                    "mkd", // North Macedonia
+                    "mli", // Mali
+                    "mlt", // Malta
+                    "mmr", // Myanmar
+                    "mne", // Montenegro
+                    "mng", // Mongolia
+                    "mnp", // Northern Mariana Islands
+                    "moz", // Mozambique
+                    "mrt", // Mauritania
+                    "msr", // Montserrat
+                    "mtq", // Martinique
+                    "mus", // Mauritius
+                    "mwi", // Malawi
+                    "mys", // Malaysia
+                    "myt", // Mayotte
+                    "nam", // Namibia
+                    "ncl", // New Caledonia
+                    "ner", // Niger
+                    "nfk", // Norfolk Island
+                    "nga", // Nigeria
+                    "nic", // Nicaragua
+                    "niu", // Niue
+                    "nld", // Netherlands
+                    "nor", // Norway
+                    "npl", // Nepal
+                    "nru", // Nauru
+                    "nzl", // New Zealand
+                    "omn", // Oman
+                    "pak", // Pakistan
+                    "pan", // Panama
+                    "pcn", // Pitcairn
+                    "per", // Peru
+                    "phl", // Philippines
+                    "plw", // Palau
+                    "png", // Papua New Guinea
+                    "pol", // Poland
+                    "pri", // Puerto Rico
+                    "prk", // Korea (Democratic People's Republic of)
+                    "prt", // Portugal
+                    "pry", // Paraguay
+                    "pse", // Palestine, State of
+                    "pyf", // French Polynesia
+                    "qat", // Qatar
+                    "reu", // Réunion
+                    "rou", // Romania
+                    "rus", // Russian Federation
+                    "rwa", // Rwanda
+                    "sau", // Saudi Arabia
+                    "sdn", // Sudan
+                    "sen", // Senegal
+                    "sgp", // Singapore
+                    "sgs", // South Georgia and the South Sandwich Islands
+                    "shn", // Saint Helena, Ascension and Tristan da Cunha
+                    "sjm", // Svalbard and Jan Mayen
+                    "slb", // Solomon Islands
+                    "sle", // Sierra Leone
+                    "slv", // El Salvador
+                    "smr", // San Marino
+                    "som", // Somalia
+                    "spm", // Saint Pierre and Miquelon
+                    "srb", // Serbia
+                    "ssd", // South Sudan
+                    "stp", // Sao Tome and Principe
+                    "sur", // Suriname
+                    "svk", // Slovakia
+                    "svn", // Slovenia
+                    "swe", // Sweden
+                    "swz", // Eswatini
+                    "sxm", // Sint Maarten (Dutch part)
+                    "syc", // Seychelles
+                    "syr", // Syrian Arab Republic
+                    "tca", // Turks and Caicos Islands
+                    "tcd", // Chad
+                    "tgo", // Togo
+                    "tha", // Thailand
+                    "tjk", // Tajikistan
+                    "tkl", // Tokelau
+                    "tkm", // Turkmenistan
+                    "tls", // Timor-Leste
+                    "ton", // Tonga
+                    "tto", // Trinidad and Tobago
+                    "tun", // Tunisia
+                    "tur", // Türkiye
+                    "tuv", // Tuvalu
+                    "twn", // Taiwan, Province of China
+                    "tza", // Tanzania, United Republic of
+                    "uga", // Uganda
+                    "ukr", // Ukraine
+                    "umi", // United States Minor Outlying Islands
+                    "ury", // Uruguay
+                    "usa", // United States of America
+                    "uzb", // Uzbekistan
+                    "vat", // Holy See
+                    "vct", // Saint Vincent and the Grenadines
+                    "ven", // Venezuela (Bolivarian Republic of)
+                    "vgb", // Virgin Islands (British)
+                    "vir", // Virgin Islands (U.S.)
+                    "vnm", // Viet Nam
+                    "vut", // Vanuatu
+                    "wlf", // Wallis and Futuna
+                    "wsm", // Samoa
+                    "yem", // Yemen
+                    "zaf", // South Africa
+                    "zmb", // Zambia
+                    "zwe", // Zimbabwe
+                ]
             }
-            */
         },
         "066": {
             "name": "",
@@ -224,7 +472,56 @@ export const validationData = {
             "validSubfields": ["*"],
             "defaultSubfields": ["b"],
             "validStrings": {
-
+                "b": [
+                    "A01",
+                    //"A02",
+                    "A03",
+                    //"A04",
+                    "A05",
+                    "A06",
+                    "A07",
+                    "A08",
+                    "A09",
+                    "A10",
+                    //"A11",
+                    "A12",
+                    "A13",
+                    "A14",
+                    "A15",
+                    "A16",
+                    "A17",
+                    "A18",
+                    "A19",
+                    "A20",
+                    "A21",
+                    "B01",
+                    "B02",
+                    "B03",
+                    "B04",
+                    "B05",
+                    "B06",
+                    "B07",
+                    "B08",
+                    "B09",
+                    "B10",
+                    "B11",
+                    "B12",
+                    "B13",
+                    "B14",
+                    "B15",
+                    "B16",
+                    "B17",
+                    "B18",
+                    "B19",
+                    "B20",
+                    "B21",
+                    "B22",
+                    "B23",
+                    "B24",
+                    "B25",
+                    "B26",
+                    "B27"
+                ]
             }
         },
         "091": {
