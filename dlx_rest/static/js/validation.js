f31076a8a4990ea3d7874869b0b17137dc1c21c8
aaronhelton <helton@un.org>
2022-12-08 09:32:51 -0500

    update validations for day 3

--- dlx_rest/static/js/validation.js
+++ dlx_rest/static/js/validation.js
@@ -1147,7 +1147,7 @@ export const validationData = {
             "validIndicators1": ["_","0","1"],
             "validIndicators2": [],
             "requiredSubfields": [],
-            "validSubfields": ["a"],
+            "validSubfields": ["a","g"],
             "defaultSubfields": ["a"] 
         },
         "710": {
@@ -1709,7 +1709,7 @@ export const validationData = {
             "validIndicators1": ["0","1","3"],
             "validIndicators2": [],
             "requiredSubfields": [],
-            "validSubfields": ["a","g"],
+            "validSubfields": ["a","g","9"],
             "defaultSubfields": ["a"] 
         },
         "110": {
