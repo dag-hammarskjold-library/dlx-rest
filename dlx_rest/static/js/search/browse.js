583018e5e660eaf47a18e34495d6e9d175e308a9
Aaron Helton <helton@un.org>
2022-12-16 12:11:48 -0500

    change browse index positioning (#890)
    * change browse index positioning
    
    * increase the limit to 50

--- dlx_rest/static/js/search/browse.js
+++ dlx_rest/static/js/search/browse.js
@@ -173,10 +173,10 @@ export let browsecomponent = {
         let matches = window.location.search.match(/type=(\w+)/)
         let recordType = this.recordType;
 
-        document.title = document.title + ` Browse (${this.recordType})`
+        let beforeBrowse = `${this.api_prefix}marc/${this.collection}/records/browse?type=${this.recordType}&search=${this.index}:${this.q}&compare=less&limit=3`
+        let afterBrowse = `${this.api_prefix}marc/${this.collection}/records/browse?type=${this.recordType}&search=${this.index}:${this.q}&compare=greater&limit=50`
 
-        let beforeBrowse = `${this.api_prefix}marc/${this.collection}/records/browse?type=${this.recordType}&search=${this.index}:${this.q}&compare=less`
-        let afterBrowse = `${this.api_prefix}marc/${this.collection}/records/browse?type=${this.recordType}&search=${this.index}:${this.q}&compare=greater`
+        document.title = document.title + ` Browse (${this.recordType})`
 
         for (let url of [beforeBrowse, afterBrowse]) {
             let resultsList = url === beforeBrowse ? this.results_before : this.results_after;
