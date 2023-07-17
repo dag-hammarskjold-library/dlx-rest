0fa8384be1a61b788ce7dc711b44c9736088238e
Aaron Helton <helton@un.org>
2023-07-17 16:38:30 -0400

    basic functionality

--- dlx_rest/static/js/import.js
+++ dlx_rest/static/js/import.js
@@ -5,18 +5,28 @@ export let importcomponent = new Vue({
     props: ["api_prefix"],
     template: `
     <div class="container">
-        <h4>Import Records</h4>
-        <div @drop.prevent @dragover.prevent @click="handleClick">
-            <div class="text-center" @dragenter="handleDragEnter" @dragleave="handleDragLeave" @drop="handleDrop" style="border: 3px dashed #dadfe3; border-radius: 15px;">
-                <h4 class="text-uppercase text-secondary mt-2" style="pointer-events:none;">Drag and Drop</h4>
-                <p class="text-center" style="pointer-events:none;">or</p>
-                <p class="text-center" style="pointer-events:none;"><a class="btn btn-success text-light">Click here to browse</a></p>
-                <input v-if="importType == 'records'" id="import" ref="import" type="file" style="opacity: 0; pointer-events:none;" @change="handleChange" :accept="accept" />
-                <input v-else id="import" ref="import" type="file" multiple="" style="opacity: 0; pointer-events:none;" @change="handleChange" :accept="accept" />
+        <div v-if="!review">
+            <h4>Import Records</h4>
+            <div @drop.prevent @dragover.prevent @click="handleClick">
+                <div class="text-center" @dragenter="handleDragEnter" @dragleave="handleDragLeave" @drop="handleDrop" style="border: 3px dashed #dadfe3; border-radius: 15px;">
+                    <h4 class="text-uppercase text-secondary mt-2" style="pointer-events:none;">Drag and Drop</h4>
+                    <p class="text-center" style="pointer-events:none;">or</p>
+                    <p class="text-center" style="pointer-events:none;"><a class="btn btn-success text-light">Click here to browse</a></p>
+                    <input v-if="importType == 'records'" id="import" ref="import" type="file" style="opacity: 0; pointer-events:none;" @change="handleChange" :accept="accept" />
+                    <input v-else id="import" ref="import" type="file" multiple="" style="opacity: 0; pointer-events:none;" @change="handleChange" :accept="accept" />
+                </div>
             </div>
         </div>
-        <div class="container">
-            <div class="row" v-for="record in records">{{record}}</div>
+        <div class="row">
+            <div class="container">
+                <div class="row" v-for="record in records">
+                    <!-- display each record, cleaning up how it appears onscreen -->
+                    <div class="col">
+                        <h4>Record: (id)</h4>
+                        <pre><small>{{record.replace(/(\\$[a-z0-9])/g, " $1 ").replace(/\\\\/g, "_")}}</small></pre>
+                    </div>
+                </div>
+            </div>
         </div>
     </div>`,
     data: function () { 
@@ -26,7 +36,8 @@ export let importcomponent = new Vue({
             // And choose different kinds of uploads we want to accept
             accept: ".mrk, .xml",
             fileList: [],
-            records: []
+            records: [],
+            review: false
         }
     },
     methods: {
@@ -58,13 +69,24 @@ export let importcomponent = new Vue({
             this.handleChange()
         },
         parse(file) {
-            console.log("Got",file)
+            /* 
+            Take the file and split it, in case it contains multiple records,
+            then attempt to validate the MARC using either jmarc.mjs (preferred)
+            OR dlx (via the dlx_rest API). Display the results onscreen 
+            (this.records) and show validation errors, as well as an import 
+            button.
+            */
+            //console.log("Got",file)
+            this.review = true
             const reader = new FileReader()
             let fileText = ""
             reader.onload = (res) => {
                 this.records = res.target.result.split("\r\s*\n")
             }
             reader.readAsText(file)
+        },
+        submit(records) {
+            /* loop through the valid records and submit each one to the endpoint */
         }
     }
 })
