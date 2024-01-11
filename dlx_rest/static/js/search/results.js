a6494e450322fc2f576cb21fefb01970420e71db
Aaron Helton <helton@un.org>
2024-01-11 16:23:13 -0500

    Handle results

--- dlx_rest/static/js/search/results.js
+++ dlx_rest/static/js/search/results.js
@@ -1,3 +1,6 @@
+import { previewmodal } from "../modals/preview.js"
+import { countcomponent } from "./count.js"
+
 export let searchresults = {
     props: {
         api_prefix: {
@@ -14,15 +17,92 @@ export let searchresults = {
     },
     template:`
     <div>
-        <div id="results-list" v-for="result in results" :key="result._id">
-            {{result}}
+        <div id="results-list" v-for="result in parsedResults" :key="result._id">
+            <div class="row mt-1 bg-light border-bottom">
+                <div class="col-sm-1">
+                    <input :id="'input-' + collection + '-' + result._id" type="checkbox" disabled="true" data-toggle="tooltip" title="Select/deselect record"/>
+                </div>
+                <div>
+                    <i :id="'preview-toggle-' + result._id"  class="fas fa-file preview-toggle" @click="togglePreview(collection, result._id)" title="preview record"></i>
+                    <div :id="'preview-' + result._id" class="record-preview hidden">
+                        <span class="record-preview-id">{{result._id}}</span>
+                        </br>
+                        <span :id="'preview-text-' + result._id" class="preview-text"></span>
+                    </div>
+                </div>
+                <div class="col-sm-9 px-4">
+                    <div v-if="collection != 'auths'" class="row" style="overflow-x:hidden">
+                        <a :id="'link-' + result._id" class="result-link" :href="uibase + '/editor?records=' + collection + '/' + result._id" style="white-space:nowrap">{{result.first_line}}</a>
+                        <countcomponent v-if="collection == 'auths'" :api_prefix="api_prefix" :recordId="result._id"></countcomponent>
+                    </div>
+                    <div v-else class="row" style="flex-wrap:inherit">
+                        <a :id="'link-' + result._id" class="result-link" :href="uibase + '/editor?records=' + collection + '/' + result._id" style="overflow-wrap:break-word">{{result.first_line}}</a>
+                        <countcomponent v-if="collection == 'auths'" :api_prefix="api_prefix" :recordId="result._id"></countcomponent>
+                    </div>
+                    <div class="row" style="white-space:nowrap">
+                        {{result.second_line}}
+                    </div>
+                    <div class="row" v-for="agenda in result.agendas">
+                        <span class="ml-3">{{agenda}}</span>
+                    </div>
+                </div>
+                <div class="col-sm-1">
+                    <!-- need to test if authenticated here -->
+                    <div class="row ml-auto">
+                        <a><i :id="'icon-' + collection + '-' + result._id" class="fas fa-2x" data-toggle="tooltip" title="Add to your basket"></i></a>
+                    </div>
+                </div>
+            </div>
         </div>
+        <previewmodal ref="previewmodal" :api_prefix="api_prefix"></previewmodal>
     </div>
     `,
     data: function () {
-        return {}
+        let myUIBase = this.api_prefix.replace('/api/','')
+        return {
+            uibase: myUIBase
+        }
+    },
+    computed: {
+        parsedResults: function () {
+            console.log(this.collection)
+            let parsedResultSet = []
+            for (let result of this.results) {
+                let myResult = { "_id": result["_id"]}
+                // Set first and second line for any collection
+                myResult["first_line"] = result["title"]
+                let rtype = result["types"].split("::")
+                myResult["second_line"] = [result["symbol"], result["date"], rtype[rtype.length - 1]].filter(Boolean).join(" | ")
+                
+                // And override for just auths
+                if (this.collection == "auths") {
+                    myResult["first_line"] = result["heading"]
+                    myResult["second_line"] = result["alt"]
+                    myResult["heading_tag"] = result["heading_tag"]
+                } 
+                
+                //if (this.collection == "files") {
+                    // not implemented yet
+                //}
+
+                if (this.collection == "speeches") {
+                    myResult["agendas"] = result["agendas"]
+                }
+
+                parsedResultSet.push(myResult)
+            }
+            return parsedResultSet
+        }
     },
     methods: {
-
+        togglePreview: async function (collection, recordId) {
+            this.$refs.previewmodal.collection = collection == "auths" ? collection : "bibs"
+            this.$refs.previewmodal.recordId = recordId
+            this.$refs.previewmodal.show()
+        },
+    },
+    components: {
+        "countcomponent": countcomponent,
+        "previewmodal": previewmodal
     }
 }
--- dlx_rest/static/js/search/search.js
+++ dlx_rest/static/js/search/search.js
@@ -30,17 +30,74 @@ export let searchcomponent = {
         </nav>
         <div v-if="showAdvanced"><advancedsearchform :collection="collection" :api_prefix="api_prefix" ref="advanced-search-form" v-on:submit="submitSearch"></advancedsearchform></div>
         <div v-else><simplesearchform :collection="collection" :api_prefix="api_prefix" ref="simple-search-form" v-on:cancel="cancelSearch" v-on:submit="submitSearch"></simplesearchform></div>
-        <!-- <pagination></pagination> -->
-        <div v-if="results.length > 0"><searchresults :collection="collection" :api_prefix="api_prefix" :results="results"></searchresults></div>
-        <!-- <pagination></pagination> -->
-
+        <div v-if="showSpinner" id="results-spinner" class="col d-flex justify-content-center">
+            <div class="spinner-border" role="status">
+                <span class="sr-only">Loading...</span>
+            </div>
+        </div>
+        <div v-if="results.length > 0">
+            <!-- <pagination></pagination> -->
+            <nav>
+                <ul class="pagination pagination-md justify-content-center">
+                    <li class="page-item disabled">
+                        <span class="page-link">
+                            {{start}} to {{end}} of 
+                            <div v-if="showCountSpinner" class="spinner-grow" role="status" style="width:1rem;height:1rem">
+                                <span class="sr-only">Loading...</span>
+                            </div>
+                            <span v-else>{{resultcount}}</span>
+                            Records ({{searchTime}} seconds)
+                        </span>
+                    </li>
+                    <li v-if="prev" class="page-item"><a class="page-link result-link" :href="prev">Previous</a></li>
+                    <li v-else class="page-item disabled"><a class="page-link result-link" href="">Previous</a></li>
+                    <li v-if="next" class="page-item"><a class="page-link result-link" :href="next">Next</a></li>
+                    <li v-else class="page-item disabled"><a class="page-link result-link" href="">Next</a></li>
+                </ul>
+            </nav>
+            <searchresults :collection="collection" :api_prefix="api_prefix" :results="results"></searchresults>
+            <!-- <pagination></pagination> -->
+            <nav>
+                <ul class="pagination pagination-md justify-content-center">
+                    <li class="page-item disabled">
+                        <span class="page-link">
+                            {{start}} to {{end}} of 
+                            <span id="result-count-bottom">
+                                <div v-if="showCountSpinner" class="spinner-grow" role="status" style="width:1rem;height:1rem">
+                                    <span class="sr-only">Loading...</span>
+                                </div>
+                                <span v-else>{{resultcount}}</span>
+                            </span>
+                            Records ({{searchTime}} seconds)
+                        </span>
+                    </li>
+                    <li v-if="prev" class="page-item"><a class="page-link result-link" :href="prev">Previous</a></li>
+                    <li v-else class="page-item disabled"><a class="page-link result-link" href="">Previous</a></li>
+                    <li v-if="next" class="page-item"><a class="page-link result-link" :href="next">Next</a></li>
+                    <li v-else class="page-item disabled"><a class="page-link result-link" href="">Next</a></li>
+                </ul>
+            </nav>
+        </div>
     </div>`,
     data: function() {
         let myUIBase = this.api_prefix.replace('/api/','')
+        /* Redo this
+        let myParams = this.search_url.split("?")[1]
+        let myProps = {}
+        for (let p of myParams.split("&")) {
+            let thisParam = p.split("=");
+            if (thisParam[0] == "start" || thisParam[0] == "limit") {
+                myProps[thisParam[0]] = parseInt(thisParam[1]);
+            } else {
+                myProps[thisParam[0]] = decodeURIComponent(thisParam[1]).replace(/\+/g, ' ');
+            }
+        }
+        */
         return {
             results: [],
             submitted: false,
             showSpinner: false,
+            showCountSpinner: false,
             hidden_qs: ["NOT 089:B22 AND NOT 089:B23"],
             searchTerm: "",
             myBasket: {},
@@ -48,7 +105,14 @@ export let searchcomponent = {
             uibase: myUIBase,
             searchTime: 0,
             showAdvanced: false,
-            actualCollection: this.collection
+            actualCollection: this.collection,
+            start: 0,
+            end: 0,
+            resultcount: 0,
+            next: null,
+            prev: null,
+            maxTime: 2000,
+            abortController: new AbortController(),
         }
     },
     created: function () {
@@ -83,24 +147,47 @@ export let searchcomponent = {
             window.history.replaceState(null, "", url)
         },
         submitSearch() {
-            // Do the search and update this.speeches
-            // Can we channel both advanced search and simple search into this?
+            // Do the search and update this.results
             let search_url = `${this.api_prefix}marc/${this.actualCollection}/records?search=${encodeURIComponent(this.qs)}&format=brief`
             console.log(search_url)
             let ui_url = `${this.api_prefix.replace("/api/","")}/records/speeches/review?q=${this.foundQ}`
             let startTime = Date.now()
             this.showSpinner = true
-            fetch(search_url).then(response => {
+            fetch(search_url, this.abortController).then(response => {
                 response.json().then(jsonData => {
                     this.results = jsonData.data
                 }).then( () => {
                     this.submitted = true
                 })
-            }).then( () => {this.showSpinner = false}).then(() => {
-                window.history.replaceState({},ui_url)
             }).then( () => {
                 this.searchTime = (Date.now() - startTime) / 1000
+                this.showSpinner = false
             })
+
+            // Removed
+            // then( () => {this.showSpinner = false}).then(() => {
+            //    window.history.replaceState({},ui_url)
+            //}).
+
+            // Now do the total result count
+            this.showCountSpinner = true
+            //this.end = this.params.start + this.params.limit -1
+            fetch(search_url.replace('/records', '/records/count'), this.abortController).then(response => {
+                response.json().then( jsonData => {
+                    this.resultcount = jsonData["data"]
+                }).then ( () => {
+                    this.showCountSpinner = false
+                    if (this.resultcount == 0) {
+                        this.start = 0;
+                    }
+                    
+                    if (this.end >= this.resultcount) {
+                        this.end = this.resultcount
+                        this.next = null
+                    }
+                })
+            })
+
         },
         cancelSearch () {
             this.abortController.abort()
