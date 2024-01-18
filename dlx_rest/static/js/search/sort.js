39a32551ed3358a0377af746c2d9efbd8783ff10
jbukhari <jb@Js-MacBook-Air.local>
2024-01-18 12:32:55 -0500

    update search results per page options

--- dlx_rest/static/js/search/sort.js
+++ dlx_rest/static/js/search/sort.js
@@ -80,10 +80,9 @@ export let sortcomponent = {
         
         return {
             rpp: [
-                {"displayName": "10", "searchString": 10},
-                {"displayName": "25", "searchString": 25},
-                {"displayName": "50", "searchString": 50},
                 {"displayName": "100", "searchString": 100},
+                {"displayName": "250", "searchString": 250},
+                {"displayName": "500", "searchString": 500},
                 {"displayName": "1000", "searchString": 1000}
             ],
             sortFields: mySortFields,
