3dd04414344d795bd27800ce7c22ec30eda8b6c0
jbukhari <joe.bukhari@gmail.com>
2024-04-11 13:42:19 -0400

    newline split (#1382)
    Co-authored-by: jbukhari <jb@Js-MacBook-Air.local>
    Co-authored-by: Aaron Helton <helton@un.org>

--- dlx_rest/static/js/import.js
+++ dlx_rest/static/js/import.js
@@ -195,8 +195,8 @@ export let importcomponent = {
             const promises = []
             this.detectedSpinner = true
             reader.onload = (res) => {
-                for (let mrk of res.target.result.split(/[\r\n]{2,}/)) {
-                    let promise = Jmarc.fromMrk(this.collection, mrk).then( jmarc => {
+                for (let mrk of res.target.result.split(/(\r\n *\r\n|\n *\n)/)) {
+                    Jmarc.fromMrk(mrk, this.collection).then( jmarc => {
                         // The only classes of validation errors we care about are:
                         // 1. Is there a duplicate symbol? If so, warn but allow import.
                         // 2. Do all the auth controlled fields match existing auth 
