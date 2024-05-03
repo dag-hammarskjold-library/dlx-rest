f85e6b181b583fe8bdef83778b85b88ff2521603
Aaron Helton <helton@un.org>
2024-05-03 15:53:25 -0400

    restore some files

---
+++ dlx_rest/static/js/search/sidebar.js
@@ -0,0 +1,8 @@
+// Only here for spacing. We can remove this when we do other refactoring.
+export let sidebarcomponent = {
+    props: ['sbtype'],
+    template: ` 
+    <div class="col-2 pt-2" :id="sbtype + '_sidebar'" style="background-color:white;">
+
+    </div>`
+}
