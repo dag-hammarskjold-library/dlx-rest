6c0eabd6d8967c879cbf29680cb1187250f002fe
jbukhari <joe.bukhari@gmail.com>
2024-06-10 23:39:27 -0400

    fixup

--- dlx_rest/api/__init__.py
+++ dlx_rest/api/__init__.py
@@ -220,13 +220,6 @@ class RecordsList(Resource):
         cls = ClassDispatch.batch_by_collection(collection) or abort(404)
         args = RecordsList.args.parse_args()
 
-        engine = "community"
-        if args.get('engine') == 'community':
-            print("Using Community search type")
-        else:
-            engine = "atlas"
-            print("Using Atlas search type")
-
         # We can also note some things about the requesting user's basket here, since this route, and all others, require login
         if not current_user.is_anonymous:
             this_u = User.objects.get(id=current_user['id'])
@@ -243,24 +236,34 @@ class RecordsList(Resource):
         
         # search
         search = unquote(args.search) if args.search else None
-
-        if args.subtype == 'all':
-            pass
-        elif args.subtype:
-            query.add_condition(Raw({'_record_type': args.subtype}))
-        else:
-            query.add_condition(Raw({'_record_type': 'default'}))
+        # subtype
+        type_condition = Raw({'_record_type': args.subtype if args.subtype else 'default'})
             
-        if engine == "community":
+        if args.engine in (None, "community"):
+            print("Using Community search type")
+
             try:
                 query = Query.from_string(search, record_type=collection[:-1]) if search else Query()
+                query.conditions.append(type_condition)
             except InvalidQueryString as e:
                 abort(422, str(e))
-        elif engine == "atlas":
+        elif args.engine == "atlas":
+            print("Using Atlas search type")
+
             try:
                 query = AtlasQuery.from_string(search, record_type=collection[:-1]) if search else AtlasQuery()
+                
+                if hasattr(query, 'match'):
+                    if query.match:
+                        query.match.conditions.append(type_condition)
+                    else:
+                        query.match = Query(type_condition)
+                else:
+                    query.match = type_condition
             except InvalidQueryString as e:
                 abort(422, str(e))
+        else:
+            query = Query({})
 
         # start
         start = 1 if args.start is None else int(args.start)
@@ -293,22 +296,10 @@ class RecordsList(Resource):
             project = {'_id': 1}
           
         # sort
-        sort_by = args.get('sort')
+        sort_by = args.get('sort') or 'updated'
         sort_by = 'subject' if sort_by == 'heading' else sort_by
         sort_by = 'symbol' if sort_by == 'meeting record' else sort_by
         sort_by = 'date' if sort_by == 'meeting date' else sort_by
-        
-        #if sort_by == 'relevance':
-        #    project['score'] = {'$meta': 'textScore'}
-        #    sort = [('score', {'$meta': 'textScore'})]
-        #elif sort_by:
-        #    sort = [(sort_by, DESC)] if (args['direction'] or '').lower() == 'desc' else [(sort_by, ASC)]
-        #    # only include results with the sorted field. otherwise, records with the field missing will be the first results
-        #    # TODO review
-        #    #query.add_condition(Raw({sort_by: {'$exists': True}}))
-        #    pass
-        #else:
-        #    sort = None
 
         # collation is not implemented in mongomock
         collation = DlxConfig.marc_index_default_collation if Config.TESTING == False else None
@@ -445,42 +436,33 @@ class RecordsListCount(Resource):
     def get(self, collection):
         cls = ClassDispatch.batch_by_collection(collection) or abort(404)
         args = RecordsList.args.parse_args()
+        type_condition = Raw({'_record_type': args.subtype if args.subtype else 'default'})
 
         if args.search:
             search = unquote(args.search)
-            
+
             if args.engine == "community":
                 try:
                     query = Query.from_string(search, record_type=collection[:-1]) if search else Query()
-                    query.add_condition(type_condition)
+                    query.conditions.append(type_condition)
                 except InvalidQueryString as e:
                     abort(422, str(e))
             elif args.engine == "atlas":
                 try:
                     query = AtlasQuery.from_string(search, record_type=collection[:-1]) if search else AtlasQuery()
 
+                    if hasattr(query, 'match'):
+                        if query.match:
+                            query.match.conditions.append(type_condition)
+                        else:
+                            query.match = Query(type_condition)
+                    else:
+                        query.match = type_condition
+
                 except InvalidQueryString as e:
                     abort(422, str(e))
         else: 
-            query = Query({})
-
-        if args.subtype == 'all':
-            # todo
-            type_condition = None
-            pass
-        elif args.subtype:
-            query.conditions.append(Raw({'_record_type': args.subtype}))
-        else:
-            query.conditions.append(Raw({'_record_type': 'default'}))
-        
-        links = {
-            '_self': URL('api_records_list_count', collection=collection, search=args.search).to_str(),
-            'related': {
-                'records': URL('api_records_list', collection=collection, search=args.search).to_str()
-            }
-        }
-        
-        meta = {'name': 'api_records_list_count', 'returns': URL('api_schema', schema_name='api.count').to_str()}
+            query = Query(type_condition)
         
         if query:
             if isinstance(query, AtlasQuery):
@@ -496,6 +478,15 @@ class RecordsListCount(Resource):
                 )
         else:
             data = cls().handle.estimated_document_count()
+        
+        links = {
+            '_self': URL('api_records_list_count', collection=collection, search=args.search).to_str(),
+            'related': {
+                'records': URL('api_records_list', collection=collection, search=args.search).to_str()
+            }
+        }
+        
+        meta = {'name': 'api_records_list_count', 'returns': URL('api_schema', schema_name='api.count').to_str()}
 
         return ApiResponse(links=links, meta=meta, data=data).jsonify()
 
