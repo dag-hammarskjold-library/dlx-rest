c97d0245cdf5922cd53fd0788e975b15be0ac176
Aaron Helton <helton@un.org>
2024-12-20 09:00:40 -0500

    show all files just like in the record editor

--- dlx_rest/static/js/recordfiles.js
+++ dlx_rest/static/js/recordfiles.js
@@ -1,18 +1,24 @@
 export let recordfilecomponent = {
     props: ["api_prefix", "record_id"],
-    template: `<div class="row" v-if="file">
-        <ul class="list-group list-group-horizontal list-group-flush m-0 p-0 pb-1">
-            <li class="list-group-item list-group-item-dark border-0 m-0 p-0 px-1">
-                en 
-            </li>
-            <li class="list-group-item list-group-item-dark border-0 m-0 p-0 px-1">
-                <a :href="file" target="_blank" title="open"><i class="fas fa-file text-dark"></i></a>
-            </li>
-        </ul>
-    </div>`,
+    template: `<ul v-if="files" class="list-group list-group-horizontal list-group-flush m-0 p-0 pb-1">
+        <li v-for="file in files" class="list-group-item border-0 m-0 p-0 mr-1 float-left">
+            <ul class="list-group list-group-horizontal list-group-flush m-0 p-0 pb-1">
+                <li class="list-group-item list-group-item-dark border-0 m-0 p-0 px-1">
+                    {{file.language}} 
+                </li>
+                <li class="list-group-item list-group-item-dark border-0 m-0 p-0 px-1">
+                    <a :href="file.url + '?action=open'" target="_blank" title="open"><i class="fas fa-file text-dark"></i></a>
+                </li>                
+                <li class="list-group-item list-group-item-dark border-0 m-0 p-0 px-1">
+                    <a :href="file.url + '?action=download'" target="_blank" title="open"><i class="fas fa-cloud-download-alt text-dark"></i></a>
+                </li>
+                
+            </ul>
+        </li>
+    </ul>`,
     data: function () {
         return {
-            file: null
+            files: null
         }
     },
     created: async function() {
@@ -21,10 +27,7 @@ export let recordfilecomponent = {
             response => response.json()
         ).then(
             json => {
-                let enFile = json.data.filter((x) => x.language == 'en')
-                if (enFile.length == 1) {
-                    this.file = enFile[0].url + "?action=open"
-                }
+                this.files = json.data
             }
         )
     }
