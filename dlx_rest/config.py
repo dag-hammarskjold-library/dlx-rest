e015cc15378208522e1cf0880195907c2f24b287
Aaron Helton <helton@un.org>
2025-02-03 11:07:08 -0500

    Bump version to v2.10.21 (#1652)
    Fixes #1651

--- dlx_rest/config.py
+++ dlx_rest/config.py
@@ -6,7 +6,7 @@ import boto3
 class Config(object):
     DEBUG = False
     TESTING = False
-    VERSION = "v2.10.20"     # Set this in each new milestone/release.
+    VERSION = "v2.10.21"     # Set this in each new milestone/release.
 
     bucket = 'undl-files'
     
