c87e9e5f6c884b025549ec82ca02a1b1007eba23
Aaron Helton <helton@un.org>
2025-03-21 10:02:23 -0400

    Use subtype to set vcoll; add voting date to vote sort; fix console errors in recordfile loading (#1685)

--- dlx_rest/static/js/search/search.js
+++ dlx_rest/static/js/search/search.js
@@ -135,7 +135,7 @@ export let searchcomponent = {
             Filter: 
             <a v-for="headFilter in headFilters" class="badge badge-light mx-1 head-filter" :data-searchString="headFilter">{{headFilter}}</a>
         </div>
-        <sortcomponent v-bind:uibase="uibase" v-bind:collection="collection" v-bind:params="params"></sortcomponent>
+        <sortcomponent v-bind:uibase="uibase" v-bind:collection="collection" v-bind:params="params" :subtype="subtype"></sortcomponent>
         <nav>
             <ul class="pagination pagination-md justify-content-center">
                 <li class="page-item disabled">
@@ -251,6 +251,7 @@ export let searchcomponent = {
             }
         }
         let myUIBase = this.api_prefix.replace('/api/', '');
+        let mySubtype = ["vote", "speech"].includes(myProps.subtype) ? myProps.subtype : this.collection
         return {
             visible: true,
             results: [],
@@ -306,7 +307,8 @@ export let searchcomponent = {
             user: null,
             collectionTitle: null,
             engine: "community",
-            previewOpen: false
+            previewOpen: false,
+            subtype: mySubtype
         }
     },
     created: async function () {
