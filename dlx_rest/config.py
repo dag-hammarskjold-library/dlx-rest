d172c526d38ec1af018fec05f9733cacb2022559
Aaron Helton <helton@un.org>
2025-04-24 12:43:42 -0400

    bump app version from 2.10.22 to 2.11 (#1727)

--- dlx_rest/config.py
+++ dlx_rest/config.py
@@ -6,7 +6,7 @@ import boto3
 class Config(object):
     DEBUG = False
     TESTING = False
-    VERSION = "v2.10.22"     # Set this in each new milestone/release.
+    VERSION = "v2.11"     # Set this in each new milestone/release.
 
     bucket = 'undl-files'
     
