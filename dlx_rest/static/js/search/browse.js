8b67d996acc24db3264df80ab8bc60d8972d1740
Aaron Helton <helton@un.org>
2025-06-17 06:00:35 -0500

    improve layout and add drag select

--- dlx_rest/static/js/search/browse.js
+++ dlx_rest/static/js/search/browse.js
@@ -12,99 +12,110 @@ export let browsecomponent = {
         index_list: { type: String, required: false }
     },
     template: `
-    <div class="col-sm-8 pt-2" id="app1" style="background-color:white;">
+    <div class="col pt-2" id="app1" style="background-color:white;">
         <div v-if="q && index">
-            <div class="row"><h3>Browsing {{displaySubtype}}/{{index}} at {{q}}</h3></div>
-            <div class="row" style="position:sticky;top:0;z-index:10;background:white;">
-                <form>
-                    <div class="form-group">
-                        <label for="searchAgain">Your search: </label>
-                        <input id="searchAgain" type="text" :value="q">
-                        <button type="button mx-2" class="btn btn-primary" @click="resubmitBrowse(index)">Search Again</button>
+            <div class="controls-header mb-3 d-flex align-items-center justify-content-between" style="position:sticky;top:0;z-index:10;background:white;">
+                <div>
+                    <h3 class="mb-0">Browsing {{displaySubtype}}/{{index}} at {{q}}</h3>
+                </div>
+                <div class="d-flex align-items-center">
+                    <div class="btn-group mr-3">
+                        <button class="btn btn-outline-secondary btn-sm" @click.prevent="selectAll">Select All</button>
+                        <button class="btn btn-outline-secondary btn-sm" @click.prevent="selectNone">Select None</button>
                     </div>
-                </form>
-            </div>
-            <div class="row" v-if="user">
-                Select 
-                <a class="mx-1 result-link" href="#" @click="selectAll">All</a>
-                <a class="mx-1 result-link" href="#" @click="selectNone">None</a>
-                <a class="mx-1 result-link" href="#" @click="sendToBasket">Send Selected to Basket (limit: 100)</a>
-            </div>
-            <div class="row">
-                <div id="before-spinner" class="col d-flex justify-content-center" v-if="loading['before-spinner']">
-                    <div class="spinner-border" role="status"><span class="sr-only">Loading...</span></div>
+                    <button v-if="selectedRecords.length > 0" 
+                            class="btn btn-primary btn-sm" 
+                            @click.prevent="sendToBasket">
+                        Send {{selectedRecords.length}} to Basket
+                    </button>
                 </div>
             </div>
-            <div v-for="(result, idx) in results_before" class="row my-2" :key="result.value + '-' + idx">
-                <div class="col-1" v-if="user">
-                    <input :id="'input-' + result.valueSafe" type="checkbox" :disabled="result.checkboxDisabled" data-toggle="tooltip" title="Select/deselect record"/>
-                    <input type="hidden" :value="result.recordId || ''" />
-                </div>
-                <div class="col">
-                    <!-- Preview icon for count == 1 -->
-                    <a v-if="result.count === 1 && result.recordId"
-                        href="#"
-                        @click.prevent="showPreview(result.recordId)"
-                        title="Preview record (read-only)"
-                        class="ml-2 text-dark">
-                        <i class="fas fa-file"></i>
-                    </a>
-                    <a :id="'link-' + result.valueSafe" :href="result.recordUrl" target="_blank" class="result-link">
-                        {{result.value}}&nbsp;
-                        <span>
-                            <span v-if="result.count !== null">({{result.count}})</span>
-                            <i v-else class="fas fa-spinner"></i>
-                        </span>
-                    </a>
-                    <br>
-                    <small><em>
-                        <span v-if="result.see || result.seeAlso">
-                            <span v-if="result.see">see: {{result.see}}<br></span>
-                            <span v-if="result.seeAlso">see also: {{result.seeAlso}}</span>
-                        </span>
-                    </em></small>
-                </div>
-            </div>
-            <div class="row">
-                <div class="col-1"></div>
-                <div class="col"><i class="fas fa-angle-double-right mr-2 text-success"></i><span class="text-success">{{q}}</span></div>
+            <div class="table-responsive">
+                <table class="table table-sm table-striped table-hover w-100" v-if="results_after.length > 0 || results_before.length > 0">
+                    <thead>
+                        <tr>
+                            <th style="width: 30px"></th>
+                            <th style="width: 30px"></th>
+                            <th style="width: 30px"></th>
+                            <th style="width: 50px">#</th>
+                            <th>Value</th>
+                            <th>Count</th>
+                            <th>See</th>
+                            <th>See Also</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr v-for="(result, idx) in [...results_before, ...results_after]" 
+                            :key="result.value + '-' + idx"
+                            @mousedown="handleMouseDown($event, result, idx)"
+                            @mousemove="handleMouseMove($event, result, idx)"
+                            @mouseup="handleMouseUp($event)">
+                            <td>
+                                <input type="checkbox"
+                                    :data-recordid="result.recordId"
+                                    v-model="result.selected"
+                                    :disabled="result.checkboxDisabled"
+                                    @change="toggleSelect($event, result)">
+                            </td>
+                            <td>
+                                <i v-if="result.locked"
+                                   :id="result.recordId + '-basket'"
+                                   class="fas fa-lock"></i>
+                                <i v-else-if="result.myBasket"
+                                   :id="result.recordId + '-basket'"
+                                   class="fas fa-folder-minus"
+                                   @click="toggleBasket($event, result.recordId)"></i>
+                                <i v-else
+                                   :id="result.recordId + '-basket'"
+                                   class="fas fa-folder-plus"
+                                   @click="toggleBasket($event, result.recordId)"></i>
+                            </td>
+                            <td>
+                                <div>
+                                    <i v-if="previewOpen === result.recordId" class="fas fa-window-close preview-toggle" v-on:click="togglePreview($event, result.recordId)" title="Preview record"></i>
+                                    <i v-else class="fas fa-file preview-toggle" v-on:click="togglePreview($event, result.recordId)" title="Preview record"></i>
+                                    <readonlyrecord v-if="previewOpen === result.recordId" :api_prefix="api_prefix" :collection="collection" :record_id="result.recordId" class="record-preview"></readonlyrecord>
+                                </div>
+                            </td>
+                            <td>{{idx + 1}}</td>
+                            <td>
+                                <a
+                                    v-if="!result.locked && result.recordId && result.count === 1"
+                                    :id="'link-' + result.recordId"
+                                    class="result-link record-title"
+                                    :href="base_url + '/editor?records=' + collection + '/' + result.recordId"
+                                >
+                                    {{result.value}}
+                                </a>
+                                <a
+                                    v-else-if="result.count > 1"
+                                    class="result-link record-title"
+                                    :href="browseSearchQuery(index, result.value, subtype)"
+                                >
+                                    {{result.value}}
+                                </a>
+                                <a
+                                    v-else
+                                    class="result-link record-title disabled"
+                                    :id="'link-' + result.recordId"
+                                    :href="base_url + '/records/' + collection + '/' + result.recordId"
+                                >
+                                    {{result.value}}
+                                </a>
+                            </td>
+                            <td>{{result.count !== null ? result.count : '' }}</td>
+                            <td>{{result.see}}</td>
+                            <td>{{result.seeAlso}}</td>
+                        </tr>
+                    </tbody>
+                </table>
             </div>
             <div class="row">
                 <div id="after-spinner" class="col d-flex justify-content-center" v-if="loading['after-spinner']">
                     <div class="spinner-border" role="status"><span class="sr-only">Loading...</span></div>
                 </div>
             </div>
-            <div v-for="(result, idx) in results_after" class="row my-2" :key="result.value + '-' + idx">
-                <div class="col-1" v-if="user">
-                    <input :id="'input-' + result.valueSafe" type="checkbox" :disabled="result.checkboxDisabled" data-toggle="tooltip" title="Select/deselect record"/>
-                    <input type="hidden" :value="result.recordId || ''" />
-                </div>
-                <div class="col">
-                    <!-- Preview icon for count == 1 -->
-                    <a v-if="result.count === 1 && result.recordId"
-                        href="#"
-                        @click.prevent="showPreview(result.recordId)"
-                        title="Preview record (read-only)"
-                        class="ml-2 text-dark">
-                        <i class="fas fa-file"></i>
-                    </a>
-                    <a :id="'link-' + result.valueSafe" :href="result.recordUrl" target="_blank" class="result-link">
-                        {{result.value}}&nbsp;
-                        <span>
-                            <span v-if="result.count !== null">({{result.count}})</span>
-                            <i v-else class="fas fa-spinner"></i>
-                        </span>
-                    </a>
-                    <br>
-                    <small><em>
-                        <span v-if="result.see || result.seeAlso">
-                            <span v-if="result.see">see: {{result.see}}<br></span>
-                            <span v-if="result.seeAlso">see also: {{result.seeAlso}}</span>
-                        </span>
-                    </em></small>
-                </div>
-            </div>
-            <!-- Preview modal -->
+            <!-- Preview modal for mobile/small screens -->
             <div v-if="previewVisible"
                 class="modal fade show d-block"
                 tabindex="-1"
@@ -156,9 +167,6 @@ export let browsecomponent = {
         </div>
     </div>
     `,
-    components: {
-        readonlyrecord
-    },
     data() {
         Jmarc.apiUrl = this.api_prefix;
         let baseUrl = this.api_prefix.replace("/api", "");
@@ -182,7 +190,12 @@ export let browsecomponent = {
             previewCollection: null,
             previewApiPrefix: null,
             previewVisible: false,
-            scrollTimout: null,
+            scrollTimeout: null,
+            previewOpen: false,
+            isDragging: false,
+            dragStartIdx: null,
+            dragEndIdx: null,
+            selectedRecords: [],
         }
     },
     computed: {
@@ -191,7 +204,17 @@ export let browsecomponent = {
                 { label: "Previous", url: this.prev },
                 { label: "Next", url: this.next }
             ];
-        }
+        },
+        browseSearchQuery() {
+            return (index, value, subtype) =>
+                this.base_url +
+                '/records/' +
+                this.collection +
+                '/search?q=' +
+                encodeURIComponent(index + ":'" + value + "'") +
+                '&subtype=' +
+                subtype;
+        },
     },
     async created() {
         const myProfile = await user.getProfile(this.api_prefix, 'my_profile');
@@ -332,45 +355,101 @@ export let browsecomponent = {
             history.pushState({}, window.location.href);
             setTimeout(() => window.location.href = targetUrl, 0);
         },
-        selectAll(e) {
-            e.preventDefault();
-            Array.from(document.getElementsByTagName("input"))
-                .filter(inputEl => inputEl.type === "checkbox" && !inputEl.disabled)
-                .forEach(inputEl => inputEl.checked = true);
+        toggleSelect(e, result) {
+            result.selected = !result.selected;
+            if (result.selected) {
+                if (!this.selectedRecords.some(r => r.record_id === result.recordId && r.collection === this.collection)) {
+                    this.selectedRecords.push({ collection: this.collection, record_id: result.recordId });
+                }
+            } else {
+                const idx = this.selectedRecords.findIndex(r => r.record_id === result.recordId && r.collection === this.collection);
+                if (idx !== -1) this.selectedRecords.splice(idx, 1);
+            }
         },
-        selectNone(e) {
-            e.preventDefault();
-            Array.from(document.getElementsByTagName("input"))
-                .filter(inputEl => inputEl.type === "checkbox")
-                .forEach(inputEl => inputEl.checked = false);
+        selectAll() {
+            [...this.results_before, ...this.results_after].forEach(result => {
+                if (!result.checkboxDisabled) {
+                    result.selected = true;
+                    if (!this.selectedRecords.some(r => r.record_id === result.recordId && r.collection === this.collection)) {
+                        this.selectedRecords.push({ collection: this.collection, record_id: result.recordId });
+                    }
+                }
+            });
+        },
+        selectNone() {
+            [...this.results_before, ...this.results_after].forEach(result => {
+                result.selected = false;
+            });
+            this.selectedRecords = [];
+        },
+        handleMouseDown(e, result, idx) {
+            if (e.button !== 0) return;
+            this.isDragging = true;
+            this.dragStartIdx = idx;
+            this.dragEndIdx = idx;
+            this.updateDragSelection();
+            document.addEventListener('mouseup', this.cancelDrag);
+        },
+        handleMouseMove(e, result, idx) {
+            if (!this.isDragging) return;
+            this.dragEndIdx = idx;
+            this.updateDragSelection();
+        },
+        handleMouseUp(e) {
+            if (this.isDragging) {
+                this.isDragging = false;
+                this.dragStartIdx = null;
+                this.dragEndIdx = null;
+                document.removeEventListener('mouseup', this.cancelDrag);
+            }
+        },
+        cancelDrag: function() {
+            this.isDragging = false;
+            this.dragStartIdx = null;
+            this.dragEndIdx = null;
+            document.removeEventListener('mouseup', this.cancelDrag);
+        },
+        updateDragSelection() {
+            let arr = [...this.results_before, ...this.results_after];
+            let [start, end] = [this.dragStartIdx, this.dragEndIdx].sort((a, b) => a - b);
+            arr.forEach((r, i) => {
+                if (!r.checkboxDisabled) r.selected = (i >= start && i <= end);
+                if (r.selected) {
+                    if (!this.selectedRecords.some(x => x.record_id === r.recordId && x.collection === this.collection)) {
+                        this.selectedRecords.push({ collection: this.collection, record_id: r.recordId });
+                    }
+                } else {
+                    const idx = this.selectedRecords.findIndex(x => x.record_id === r.recordId && x.collection === this.collection);
+                    if (idx !== -1) this.selectedRecords.splice(idx, 1);
+                }
+            });
         },
         sendToBasket(e) {
             e.preventDefault();
-            const items = [];
-            let idx = 0;
-            Array.from(document.getElementsByTagName("input"))
-                .filter(inputEl => inputEl.type === "checkbox" && inputEl.checked)
-                .forEach(inputEl => {
-                    if (idx < 100) {
-                        const record_id = inputEl.nextElementSibling.value;
-                        items.push({ collection: this.collection, record_id });
-                        idx++;
-                    }
-                });
+            const items = this.selectedRecords.slice(0, 100);
             if (items.length > 0) {
                 basket.createItems(this.api_prefix, 'userprofile/my_profile/basket', JSON.stringify(items))
                     .then(() => window.location.reload(false));
             }
         },
-        showPreview(recordId) {
-            this.previewRecordId = recordId;
-            this.previewCollection = this.collection;
-            this.previewApiPrefix = this.api_prefix;
-            this.previewVisible = true;
+        togglePreview(event, recordId) {
+            if (event.target.classList.contains("preview-toggle") && this.previewOpen === recordId) {
+
+                this.previewOpen = false;
+            } else if (recordId) {
+                this.previewOpen = recordId;
+            } else {
+                this.previewOpen = false;
+            }
+
+            return
         },
-        closePreview() {
-            this.previewVisible = false;
-            this.previewRecordId = null;
+        dismissPreview: function () {
+            for (let d of document.getElementsByClassName("preview")) {
+                if (!d.classList.contains("hidden")) {
+                    d.classList.toggle("hidden")
+                }
+            }
         },
         handleScroll() {
             if (this.scrollTimeout) clearTimeout(this.scrollTimeout);
@@ -385,5 +464,8 @@ export let browsecomponent = {
                 }
             }, 200); // 200ms debounce
         },
+    },
+    components: {
+        readonlyrecord
     }
 }
