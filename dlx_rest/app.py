ece308901828f57c3c0de851c5ec9971146db18e
Aaron Helton <helton@un.org>
2025-06-30 15:43:48 -0400

    use dummy roots for alternative deploy paths

--- dlx_rest/app.py
+++ dlx_rest/app.py
@@ -1,6 +1,7 @@
 from flask import Flask, Response, url_for, jsonify, abort as flask_abort, session, send_from_directory
 #from flask_restx import Resource, Api, reqparse
 from flask_login import LoginManager
+from werkzeug.middleware.dispatcher import DispatcherMiddleware
 from mongoengine import connect, disconnect
 from mongomock import MongoClient as MockClient
 from flask_cors import CORS
@@ -68,6 +69,14 @@ try:
     app.secret_key=Config.secret_key
 except AttributeError:
     app.secret_key='top secret!'
+
+# Dummy root routes for dev and uat environments
+dev_app = DispatcherMiddleware(Flask('dummy_root'), {
+    '/dev-editor': app,
+})
+uat_app = DispatcherMiddleware(Flask('dummy_root'), {
+    '/uat-editor': app,
+})
 
 # Main app routes
 from dlx_rest.routes import *
